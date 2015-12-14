209d98c517f8eee6cb10b8eccc60b9685a970b5c
Soichiro-Yoshimura <soichiro_yoshimura@dwango.co.jp>
2015-12-14 17:38:57 +0900

    first commit

---
+++ test.js
@@ -0,0 +1,10 @@
+(function () {
+	'use strict';
+	let seventeen = require('./index.js');
+	let assert = require('assert');
+	
+	let a = [83, 32, 85, 47, 77, 8, 61, 74, 29, 34, 11, 76, 60, 99, 55, 7, 19, 60, 98, 38, 28, 96, 32];
+	assert.deepEqual(a.filter(seventeen.select), [85, 34]);
+
+	console.log('テストが正常に完了しました');
+})();
