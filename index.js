ad4cf6af072aa86c96666beead6c0bbdf3dc23cc
Davideryu_Orihara <davideryu_orihara@dwango.co.jp>
2015-12-25 07:25:02 +0000

    isSeventeen 関数の実装

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 (function () {
 	'use strict';
+	
+	function isSeventeen(value) {
+		 return value % 17 === 0;
+	}
 
 	module.exports = {
+		isSeventeen : isSeventeen
 	}
 })();
--- test.js
+++ test.js
@@ -4,7 +4,7 @@
 	let assert = require('assert');
 	
 	let a = [83, 32, 85, 47, 77, 8, 61, 74, 29, 34, 11, 76, 60, 99, 55, 7, 19, 60, 98, 38, 28, 96, 32];
-	assert.deepEqual(a.filter(seventeen.select), [85, 34]);
+	assert.deepEqual(a.filter(seventeen.isSeventeen), [85, 34]);
 
 	console.log('テストが正常に完了しました');
 })();
