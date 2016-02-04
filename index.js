8404684ed77410aa58fc9339d945d38521cb436c
Shutaro Takimoto <shutaro_takimoto@dwango.co.jp>
2016-02-04 08:20:14 +0000

    isSeventeen 関数を実装

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 (function () {
 	'use strict';
+    
+    /**
+     * 整数が 17 で割り切れるかどうかを判定する
+     * @param {number} n
+     * @return {boolean}
+     */
+    function isSeventeen(n) {
+        return n % 17 === 0;
+    }
 
 	module.exports = {
+        isSeventeen: isSeventeen
 	}
 })();
