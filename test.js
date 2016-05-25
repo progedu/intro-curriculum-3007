9506dc6d422c35f0cb2317098e8b2bb8603f9fae
y-takashina <takashina2051@gmail.com>
2016-05-25 16:55:29 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 (function () {
 	'use strict';
-	let seventeen = require('./index.js');
-	let assert = require('assert');
+	const seventeen = require('./index.js');
+	const assert = require('assert');
 	
-	let a = [83, 32, 85, 47, 77, 8, 61, 74, 29, 34, 11, 76, 60, 99, 55, 7, 19, 60, 98, 38, 28, 96, 32];
+	const a = [83, 32, 85, 47, 77, 8, 61, 74, 29, 34, 11, 76, 60, 99, 55, 7, 19, 60, 98, 38, 28, 96, 32];
 	assert.deepEqual(a.filter(seventeen.isSeventeen), [85, 34]);
 
 	console.log('テストが正常に完了しました');
