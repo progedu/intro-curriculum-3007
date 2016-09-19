74673cf40ba45fc044ce10a581815d5c545c42b3
Yukari Kakuta <girafflyn@gmail.com>
2016-09-19 09:57:58 +0000

    isSeventeen関数の実装

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 (function () {
 	'use strict';
+	/**
+	 * 17で割り切れるものはtrueを返す
+	 * @param {number} num
+	 */
+	function isSeventeen(num) {
+		return num % 17 === 0;
+	} 
 
 	module.exports = {
+		isSeventeen: isSeventeen
 	}
 })();
