3d49db62e183d9908ce52aad49f73351bbf7a59f
ninja6331 <ninja6331@gmail.com>
2016-09-20 12:56:07 +0000

    isSeventeen モジュールの実装

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-(function () {
 	'use strict';
 
-	module.exports = {
+	/**
+	 * isSeventeen 関数の実装
+	 * 整数が 17 で割り切れるかどうかを判定
+	 */
+
+	function isSeventeen(num) {
+		return num % 17 === 0;
 	}
-})();
+
+
+	module.exports = {
+		isSeventeen: isSeventeen
+	}
