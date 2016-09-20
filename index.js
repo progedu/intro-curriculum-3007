21a49a3175f341e59b4234b36a3f3b92d524dfa7
Chagi <amenbows@yahoo.co.jp>
2016-09-20 22:17:39 +0900

    isSeventeen モジュールの実装

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
+/**
+* ˆø”‚Ì’l‚ª 17 ‚ÅŠ„‚èØ‚ê‚é®”‚Å‚ ‚é‚Æ^‚ğ•Ô‚·
+* @param {number} num
+* @return {boolean} 17 ‚ÅŠ„‚èØ‚ê‚½‚©‚Ç‚¤‚©
+*/
+function isSeventeen (num) {
+	return num % 17 === 0;
+}
+
 module.exports = {
+	isSeventeen: isSeventeen
 }
