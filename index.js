2586c1d570c6751d67db3888e195145362981b1a
mskblur <muneo551@gmail.com>
2016-12-02 00:29:17 +0900

    isSeventeen モジュールの実装

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
 
-module.exports = {
+function isSeventeen(number){
+    if (number % 17 == 0){
+        return true;
+    }
+    return false;
 }
+
+module.exports = {
+    isSeventeen: isSeventeen
+};
