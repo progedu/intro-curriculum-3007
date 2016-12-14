ed97d23b4870776ebd0acd830a310dafa94647a3
ie <ie_kara_demasen@yahoo.co.jp>
2016-12-14 08:53:11 +0000

    isSeventeen関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 'use strict';
 
-module.exports = {
+/**
+ * 与えられた数字が17で割り切れるかどうか判定する
+ * @param {number} number
+ * @return {number} number
+ */
+function isSeventeen(number) {
+  if (number % 17 === 0) {
+    return number;
+  } else {
+    return;
+  }
 }
+
+module.exports = {
+  isSeventeen: isSeventeen
+};
