8d964d82c5d29456f8510a2e5fce238b8c502e90
Akimoto Daichi <daichi_akimoto@dwango.co.jp>
2016-12-27 08:12:29 +0000

    17の倍数か判定する関数 isSeventeen 実装

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
-
-module.exports = {
+/**
+ * 整数が17で割り切れるか判定する
+ * @param {number} n
+ * @return {boolean} 割り切れるかどうか
+ */
+function isSeventeen(n) {
+    return n % 17 === 0;
 }
+module.exports = {
+    isSeventeen : isSeventeen
+};
