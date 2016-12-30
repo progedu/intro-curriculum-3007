6a05084f5fb1453348c99ba85b85554d4b295f21
elliot215 <elliot215@gmail.com>
2016-12-30 10:57:12 +0000

    seventeen モジュールの実装

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
+/**
+ * 17 で割り切れる場合、True を返します
+ * @param {number} num
+ */
+function isSeventeen(num) {
+    return (num % 17 === 0);
+}
+
 module.exports = {
+    isSeventeen: isSeventeen
 }
