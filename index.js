e5e6ccc8f5d764720bfb3162440aa66fb69e1a58
Breezman <kon1yaga2yamada3@gmail.com>
2017-01-10 02:12:14 +0000

    isSeventeen関数のモジュールの実装

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 'use strict';
 
+function isSeventeen(num) {
+
+    if(num % 17 === 0) {
+
+        return num ;
+    } else {
+
+        return 0;
+    }
+
+
+}
+
 module.exports = {
+    isSeventeen: isSeventeen
 }
