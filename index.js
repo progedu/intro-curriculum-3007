8d062c69b0fc9b28102df267abeee729d4fc07db
twistergame <bdcb787@yahoo.co.jp>
2017-01-14 08:29:42 +0000

    isSeventeenの実装

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
 
+/**
+ * 整数が17で割り切れる場合trueを返す
+ * @param {number} num
+ */
+function isSeventeen(num) {
+    return num % 17 === 0;
+}
 module.exports = {
+    isSeventeen: isSeventeen
 }
