31a2f6fc5330f02967f29b3dbd91926151fd362e
KaiKogure <kai.kogure@live.jp>
2017-02-08 14:08:36 +0000

    seventeen function added

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 'use strict';
-
+/**
+ * 17の倍数である場合のみ　trueを返す
+ * @param {number} num
+ */
+function isSeventeen(num){
+    return (num %17 === 0);
+}
 module.exports = {
+    isSeventeen: isSeventeen
 }
