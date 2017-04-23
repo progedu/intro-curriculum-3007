71b8a6315264037fbcf5105712c0598c70955589
Spadefang <spadefang@gmail.com>
2017-04-23 04:18:07 +0000

    演習回答：isSeventeen

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
-module.exports = {
+/**
+ * 数値を受け取って17で割り切れるならtrue, でないならfalse返却
+ * @param {int} num 
+ * @return {boolean}
+ */
+function isSeventeen(num) {
+    return num % 17 === 0;
 }
+
+module.exports = {
+    isSeventeen: isSeventeen
+};
