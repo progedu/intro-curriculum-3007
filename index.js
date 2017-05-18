51f8ee50e37c4b88df7d4161d041964c8ee4c73e
kota2neko <tm2tsk54@yahoo.co.jp>
2017-05-18 11:17:37 +0000

    isSeventeen モジュールの実装

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
-
+/**
+ * 与えられた整数が 17 で割り切れれば true を返す関数 isSeventeen
+ * @param {number} n 
+ */
+function isSeventeen(n) {
+    var rem = n % 17;
+    if (rem === 0) {
+        return true;
+    }
+} // まさか１行で書けるとは・・・ orz
 module.exports = {
+    isSeventeen: isSeventeen
 }
