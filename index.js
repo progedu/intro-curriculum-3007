81e0e6a31276903ff65b9eb611c3d5e130f875f1
mk46sm <kento_17n1200073@nnn.ed.jp>
2017-06-14 12:30:12 +0900

    seventenn モジュール

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
+/**
+ * 17の倍数の時 true を返す
+ * @param {num} num
+ */
+
+function isSeventeen(num) {
+    return num % 17 === 0;
+}
 
 module.exports = {
-}
+    isSeventeen : isSeventeen
+};
