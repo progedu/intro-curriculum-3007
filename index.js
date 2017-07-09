516eb7ea54f041b119ea3ddbd428035d7b99154d
shunta071 <2095071@gmail.com>
2017-07-09 18:12:39 +0900

    17の倍数の整数

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
+/**
+ *  17の倍数である場合trueを返す
+ * @param {number} num
+ */
+
+function isSeventeen(num) {
+    return num % 17 === 0;
+}
 
 module.exports = {
+    isSeventeen: isSeventeen
 }
