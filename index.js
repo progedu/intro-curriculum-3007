0f508dc858d88f1f57b2163c1fc09b2fc9e25306
storm117 <keitarou_17n1100964@nnn.ed.jp>
2017-07-11 12:05:41 +0900

    17が何たらかんたら

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
+/**
+* 17の倍数である場合 true を返す
+* @param {number} num
+*/
+function isSeventeen(num) {
+  returm num % 17 === 0;
+}  
 
 module.exports = {
-}
+  isSeventeen: isSeventeen
+};
