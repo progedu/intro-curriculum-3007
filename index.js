e748f24b1851a6ffa6724b7c6a12d7e75cafa1d1
meganeneko <ge2hoka@yahoo.co.jp>
2017-09-09 12:41:59 +0000

    17の倍数テスト実装

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 'use strict';
-
+/**
+* 17の倍数である整数に true を返す
+* @param {number} num
+*/
+function isMultipleOfSeventeen(num) {
+    return num % 17 === 0;
+}
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
