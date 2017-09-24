782fc2c50feaddb975f3f501ac8c1c664b5a2ac0
rinringo <rintarou_17n1101688@nnn.ed.jp>
2017-09-24 14:37:23 +0000

    isMultipleOfSeventeenの実装

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
+const tasks = new Map();
+
+function isMultipleOfSeventeen(num){
+ if(num % 17=== 0){
+     return num;
+ }
+}
+
 
 module.exports = {
+    isMultipleOfSeventeen:isMultipleOfSeventeen
 }
