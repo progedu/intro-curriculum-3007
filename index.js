259cb62ec29fa97bd8b3c3fe0c8cd4f42df1dfde
minimoog93 <minicat3090tan@gmail.com>
2017-09-25 05:04:32 +0000

    モジュールに実装を追加

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
+/**
+* 17の倍数ならば true
+* @param {number} n 
+*
+*/
+
+function isMultipleOfSeventeen(n) {
+    return n % 17 === 0;
+}
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
