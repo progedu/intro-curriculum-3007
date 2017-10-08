c47fc7d35517f94754288ed90150b087cb055502
gadget-tf <gadget.tf@gmail.com>
2017-10-08 14:49:58 +0000

    練習問題の解答の実装

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
+/**
+ * 17で割り切れるかどうかをチェックする
+ * @param {number} number 
+ * @return {boolean} 17で割り切れる場合:true 割り切れない場合:false
+ */
+function isMultipleOfSeventeen(number) {
+    return (number % 17) === 0;
+}
+
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
