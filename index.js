651310689e798034bf48498a5da72ef627a62df7
Yoshihisa Masuda <yossoon@gmail.com>
2017-10-21 14:20:56 +0000

    isMultipleOfSeventeen implemented

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
-
+/**
+ * 17で割り切れるかどうかを判定して真偽値を返す
+ * @param {Int} n 
+ * @return {boolean} 割り切れるかどうか
+ */
+function isMultipleOfSeventeen(n) {
+    return n % 17 === 0;
+}
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
