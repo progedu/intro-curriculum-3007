eb02df9c0679f222182501fdf53e3851795247a0
Shigeta Asano <asano@nhn-playart.com>
2017-10-25 09:34:40 +0000

    17で割り切れるか判定するモジュールの実装

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 'use strict';
 
-module.exports = {
+/**
+ * @param {int} n
+ * @return {boolean} 17で割り切れる場合trueを返す
+ */
+function  isMultipleOfSeventeen(n) {
+    if ((n % 17) === 0){
+        return true;
+    }else{
+        return false;
+    }
 }
+
+module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
+};
