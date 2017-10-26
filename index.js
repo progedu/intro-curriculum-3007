9453b197d340797ef1e7204d157301b6caf47cf6
kanayo1993 <kanayo1993@gmail.com>
2017-10-26 05:50:09 +0000

    write seventeen function and fix test.js

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 'use strict';
+/*
+const ansNums = new Map();
+
+function isSeventeen(nums){
+    for(let num of nums){
+        if (num % 17 === 0){
+            ansNums.set(num);
+        }
+    }
+    return ansNums;
+}
+考えすぎた・・・ 
+*/
+function isSeventeen(num){
+    return num % 17 === 0;
+}
+
 
 module.exports = {
-}
+    /*seventeen: seventeen*/
+    /**  「isSeventeen 関数を seventeen モジュールに実装」という言葉に惑わされてしまいました。 */
+    isSeventeen: isSeventeen
+};
