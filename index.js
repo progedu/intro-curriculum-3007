c72acb63dd600619a0cd4fdedd81b2ac830eda96
hipela <lhyphor.mek@gmail.com>
2017-10-27 22:58:05 +0000

    seventeenモジュールによる17の倍数の選別

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
+/**
+ * 17の倍数で割り切れる値を選別する
+ * @param {number} num
+ */
+function isMultipleOfSeventeen(num) {
+    return num % 17 === 0;
+}
+
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
