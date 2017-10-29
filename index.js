099bc894fdb756b0c29c4f4bfc0012c1d108bc06
s1lmer1a <you@sample.com>
2017-10-29 14:52:39 +0000

    Add isMultipleOfSeventenn func

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 'use strict';
 
+/**
+ * 17で割り切れたら整数を返す
+ * @param {int} num
+ * @return {boolean}
+ */
+function isMultipleOfSeventeen(num) {
+    if (num % 17 === 0) {
+        return true;
+    }
+}
+
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
