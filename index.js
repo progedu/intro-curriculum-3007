1e4a7e2186d4d1fe8a75133ee0626337051a2b9e
deboracaffe <yshr.kiyohara@gmail.com>
2017-11-05 12:30:34 +0000

    isMultipleOfSeventeenの実装

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 'use strict';
 
+//整数が１７でわりきれるかどうかを判定する
+function isMultipleOfSeventeen(value) {
+    return value % 17 === 0;
+}
+
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
