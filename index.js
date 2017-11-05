8405c7a2aac09316c404672c3bf7de77c152d508
Kazuya Saito <kazuya.saito.mirai@gmail.com>
2017-11-05 05:34:55 +0000

    seventeen

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 'use strict';
 
+
+/**
+ *
+ * @param n
+ * @return {boolean}
+ */
+
+function isMultipleOfSeventeen(n) {
+    return  n % 17 === 0;
+}
+
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
