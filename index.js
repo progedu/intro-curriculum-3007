d85dab23b3ecd35076fcfc18e254839bda813032
mac <m.yoshitsugu@primestyle.co.jp>
2017-11-10 08:57:09 +0900

    seventeenモジュールに実装

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 'use strict';
+//key: タスクの文字列 value: 完了しているかどうかの真偽値
+const tasks = new Map();
+
+/**
+* 配列を取り出し、17で割れる時吐き出す
+* @param {string} task
+*/
+function isMultipleOfSeventeen(x) {
+	if (x % 17 === 0) {
+		return x;
+	}
+}
 
 module.exports = {
-}
+	isMultipleOfSeventeen: isMultipleOfSeventeen
+};
