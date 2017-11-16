8eeef5d1fb82d007b9097778ecb582bba4ce0c38
by26feb <cha202802@gmail.com>
2017-11-16 23:29:07 +0000

    implement isMultipleOfSeventeen function

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 'use strict';
 
+/**
+ * 17の倍数かどうか判別します
+ * @param {number} num
+ * @returns {boolean}
+ */
+function isMultipleOfSeventeen(num) {
+  return isMultipleOf(17, num);
+}
+
+function isMultipleOf(coef, num) {
+  return num % coef === 0;
+}
+
 module.exports = {
+  isMultipleOfSeventeen: isMultipleOfSeventeen
 }
