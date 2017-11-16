5cd2095c6b280fbb5a31f7c3776a318ca4a27540
UnaagiEel <reo_16n3100040@nnn.ed.jp>
2017-11-16 06:18:27 +0000

    isMultipleOfSeventeen関数を実装

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
 
+/**
+ * isMultipleOfSeventeen関数
+ */
+function isMultipleOfSeventeen(num) {
+    return num % 17 === 0;
+}
+
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
