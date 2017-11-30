4b28f1041456ca04a7001bb4cc87aa990e78cec0
torunekonohito <torunekonohito@gmail.com>
2017-11-30 00:29:03 +0000

    isMultipleOfSeventeen関数を実装

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
+/**
+ * 17で割り切れるかどうかを判定する
+ * @param {int} i
+ */
+function isMultipleOfSeventeen(i){
+    return ( i % 17 ) === 0;
+}
+
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
