97489f1ddda6f3af6293676e6f29f38e5a221b8f
SHNysk <show.nobile@gmail,com>
2017-12-04 16:11:22 +0000

    isMultipleOfSeventeen関数の処理を修正

--- index.js
+++ index.js
@@ -2,14 +2,11 @@
 
 /**
  * 整数が17で割り切れるかどうかを判定する
- * @param {int} number
+ * @param {number} num
  * @return {boolean}
  */
-function isMultipleOfSeventeen(number) {
-    if (number % 17 == 0) {
-        return true;
-    }
-    return false;
+function isMultipleOfSeventeen(num) {
+    return num % 17 === 0;
 }
 
 module.exports = {
