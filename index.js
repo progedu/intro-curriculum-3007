38f4be7c863b4c12df25ad06535e6cbbc4bf5d4b
str-kwbt <setsurakawabata@gmail.com>
2017-12-10 02:37:31 +0000

    isMultipleOfSeventeen 関数を実装

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
-module.exports = {
+/**
+ * 整数が17で割り切れる場合、trueを返す
+ * @param {number} num
+ */
+function isMultipleOfSeventeen(num) {
+    return num % 17 === 0;
 }
+
+module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
+};
