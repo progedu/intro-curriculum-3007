c86222e29e6c373b3802841f4994c13a44f4cd0b
apprec100 <apprec100@outlook.com>
2017-12-11 04:44:41 +0000

    17で割り切れるかを判定する関数を実装

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
+/**
+* 整数を受け取り、17 で割り切れるか否かを返す
+* @param {number} num
+* @return {boolean} 17 で割り切れるか否か
+*/
+function isMultipleOfSeventeen(num) {
+    return num%17===0;
+}
+
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
