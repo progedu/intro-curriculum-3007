e01b2f28da6ecaf472db364f528614b40534cc30
minatooe <minatooe@gmail.com>
2017-12-16 11:54:37 +0000

    17で割り切れる判定実装

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 'use strict';
 
+/**
+ * 与えられた数が17で割り切れるかどうかを返す
+ * @param{init} n
+ * @return{boolean}
+ */
+
+function isMultipleOfSeventeen(n){
+    if (n % 17 === 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
