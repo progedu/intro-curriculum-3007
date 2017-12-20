6b222ccd334b5e96aeb6a3ac9549f497908026e0
Akira KAKINOHANA <kira@kirameister.net>
2017-12-20 00:06:07 +0000

    練習問題の解答

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 'use strict';
 
-module.exports = {
+/* 
+* 整数が 17 で割り切れるかどうかを判定する
+* @param {number} n
+* @return {boolean}
+*/
+
+function isMultipleOfSeventeen(n){
+    return n % 17 === 0;
 }
+
+module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
+}
+
