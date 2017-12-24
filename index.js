79153ee36a3d22330ec4545f4f39cddf25344c88
Kaito Sato <skt1523@gmail.com>
2017-12-24 06:24:20 +0000

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
+/**
+ * 配列に含まれる整数が17で割り切れるかを判定する
+ * @param {number} num 
+ */
+function isMultipleOfSeventeen(num) {
+    return num % 17 === 0;
+}
+
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
