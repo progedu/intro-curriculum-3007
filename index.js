de1ff200b2377202713b15be3c1594cdf8e5e8aa
hagiwara <keep.my.best.61@gmail.com>
2017-12-29 08:20:04 +0000

    17で割り切れるかどうか判定する関数の実装完了

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
 
+/**
+ * @param {int} num
+ */
+function isMultipleOfSeventeen(num) {
+  return num % 17 === 0;
+}
+
 module.exports = {
+  isMultipleOfSeventeen: isMultipleOfSeventeen
 }
