13fc98aaf4740fc9783def83dca2447e746cea42
Tommy Hirata <hirata.tommy@gmail.com>
2017-12-30 03:43:08 +0000

    Add isMultipleOfSeventeen

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
+
+/**
+ * 17の倍数であるならtrueを返す
+ * @param {number} number 
+ */
+function isMultipleOfSeventeen(number) {
+    return number % 17 === 0;
+}
+
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen    
 }
