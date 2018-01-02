cbc83ad3c64641bb3a2cd494b3d76cbe136e0fd3
yunosuke <h.yuunosuke@gmail.com>
2018-01-02 11:17:05 +0000

    implemented isMultipleOfSeventeen function

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 'use strict';
 
+/**
+ * multiple of seventeen
+ * @param {array} numbers
+ * @return boolean
+ */
+
+function isMultipleOfSeventeen(num) {
+  return num % 17 === 0;
+}
+
 module.exports = {
+  isMultipleOfSeventeen: isMultipleOfSeventeen
 }
