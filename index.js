306960a61292cdd86669e41c60dde669c0f623d9
watahiki606 <v10acdict@gmail.com>
2018-01-03 20:26:17 +0900

    回答

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
+/**
+ * 受け取った配列に含まれる整数が17で割り切れるかを返す
+ * @param {intNumber} 整数
+ * @return {boolean} 17で割り切れるかどうか
+ */
+function isMultipleOfSeventeen(intNumber) {
+        return intNumber % 17 === 0;
+    };
+
 module.exports = {
-}
+    isMultipleOfSeventeen: isMultipleOfSeventeen
+};
