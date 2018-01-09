f5562d12f35a35cdc1cd2d274de2dcbf6a01255f
susumutomita <oyster880@gmail.com>
2018-01-08 23:50:38 -0800

    モジュール化された処理練習問題の回答

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
+/**
+* 整数が 17 で割り切れるかどうかを判定する入力値を17で割った時の余りが 0 になる時だけ true を返す
+* @param {number} number
+* @return {boolean} 17で割り切れるかどうかの真偽値
+*/
+function isMultipleOfSeventeen(number) {
+    return number % 17 === 0
+}
+
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
