ebedf2d2d02d764500e7971aad47053a6a42e93a
h.kawanami <h.kawa@cap.ocn.ne.jp>
2018-01-14 13:55:01 +0000

    isMultipleOfSeventeenを実装

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 'use strict';
+/**
+ * 正の整数を受け取り、17の倍数かどうかを返す
+ * @param {number} num 
+ * @return {boolean} multiples of seventeen
+ */
+function isMultipleOfSeventeen(num){
+    if (num % 17 === 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
