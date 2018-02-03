f96b87ac08ec962e92fa98b44c382dd0bf24c642
Yuji Koyama <gladiolus_vstl@gladiolus.egoism.jp>
2018-02-03 02:25:07 +0000

    17で割れるかどうかを判定するisMultipleOfSeventeen関数を実装

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
+/**
+ * 引数が17の倍数かどうかを判定する
+ * @param {number} num
+ */
+function isMultipleOfSeventeen(num){
+    return (num % 17 === 0);
+}
 
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
