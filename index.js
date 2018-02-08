c76e54bfd706de2406d27297e2d14cc429cfd6ed
hiroki <zgmshiroki@gmail.com>
2018-02-08 14:07:27 +0000

    commit完了

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
+/**
+ * 17の倍数で割り切れる整数のみtrueにする
+ * @param {number} num
+ * @return {boolean}
+ */
+function isMultipleOfSeventeen(num){
+    return num % 17 === 0;
+}
+
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
