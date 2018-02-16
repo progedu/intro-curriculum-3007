b6f2dcdda261f450fbf98497ef2c4bcddfce75ab
kzin <katesuzuki0113@gmail.com>
2018-02-16 06:43:45 +0000

    実装しました

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
 
+function isMultipleOfSeventeen(num) {
+    if(num % 17 === 0){
+        return true;
+    }
+    return false;
+}
+
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
