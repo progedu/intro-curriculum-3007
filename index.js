040049104d9bc3f2a2960b6ddd1405aeb003225b
fugin <gitKawa5866@gmail.com>
2018-02-26 12:46:28 +0000

    seventeenモジュールの実装

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
+/**
+ * 17の倍数の場合、true
+ * @param {number} num
+ */
+function isMultipleOfSeventeen(num) {
+    return num % 17 === 0
+}
 
 module.exports = {
-}
+    isMultipleOfSeventeen: isMultipleOfSeventeen
+};
