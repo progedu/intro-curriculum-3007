28b2d21a883c777b83622ac0d8e622a5e6f2fe37
seiya9393 <dmc_drcseiya@yahoo.co.jp>
2018-03-01 09:29:01 +0000

    isMultipleOfSeventeenの実装

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 'use strict';
 
+/**
+ * 数字を受け取り、17で割り切れるかどうかを返す
+ * @param {number} num 
+ * @return {boolean} 17で割り切れるかどうか
+*/
 function isMultipleOfSeventeen(num) {
-    return num % 17 === 0
+    return num % 17 === 0;
 }
 
 module.exports = {
