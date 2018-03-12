f1e3362fed9ec99c5a972c2250aef8dd2bc616dd
tetsuro-nakmura <nakamur23jp@yahoo.co.jp>
2018-03-12 04:09:46 +0000

    17で割りきれる関数実装

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
+/**
+ * 17の倍数の場合に真を返す関数　17で割りあまり0
+ * @param {number} num  数値型の引数
+ */
+
+
+function  isMultipleOfSeventeen(num) {
+    return num % 17 === 0;   
+}
 
 module.exports = {
-}
+    isMultipleOfSeventeen: isMultipleOfSeventeen
+};
