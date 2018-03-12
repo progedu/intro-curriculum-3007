8b2932e9dd817e43ff025f24cd1e7b5a0373ccbe
nanae <desperado.kazune@gmail.com>
2018-03-12 04:38:53 +0000

    isMultipleOfSeventeen関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
+/**
+ * 整数nが17で割り切れるかを判定する
+ * @param {number} n 整数
+ * @return {boolean} 17で割り切れるかどうかの真偽値
+ */
+function isMultipleOfSeventeen(n) {
+    return n % 17 == 0;
+}
+
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen,
 }
