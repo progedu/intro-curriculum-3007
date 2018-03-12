21f3f4dd601bf6baa0e6b97b087e596f699b96bd
Yoshiki Nakahara <szk24nakahara@yahoo.co.jp>
2018-03-12 23:22:01 +0000

    わかりませんでした。

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
+const seventeen = new Map();
+
+function isMultipleSeventeen(numbers) {
+     number = numbers % 17 === 0;
+     rerurn seventeen.set(number);
+}
+
 
 module.exports = {
+    isMultipleSeventeen: isMultipleSeventeen
 }
