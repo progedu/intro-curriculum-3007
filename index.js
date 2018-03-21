cc183486c8155cf7ce23c24b67744b5d199854ec
Keita Sakurazawa <clearnesscircle@gmail.com>
2018-03-21 20:56:04 +0900

    17で割り切れるものだけにする

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
+/**
+ * 配列に含まれる整数が17で割り切れるか返す
+ * @param {number} num
+ * @return {boolean}
+ */
+function isMultipleOfSeventeen(num) {
+    return num % 17 === 0;
+}
+
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
