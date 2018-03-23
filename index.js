3c7da53bab610ba5c04d56f42c59e2de05d3e1fc
tsudukihashi0817@gmai.com <tsudukihashi0817@gmail.com>
2018-03-23 12:49:56 +0900

    isMultipleOfSeventeen関数の実装

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 /**
  * 配列に含まれる整数が17で割り切れるものだけにする
  * @param {number} num 
- * @return {number} num%17===0
+ * @return {bool} num % 17 === 0
  */
 function isMultipleOfSeventeen(num){
     return num % 17 === 0;
