a224a0ab585f9158274321cea39fe295cb21634e
ocknamo <s14pes@gmail.com>
2018-04-06 19:17:38 +0900

    整数が17でわり切れるかどうかを判定する関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
+/**
+ * 整数が 17 で割切れるかどうかを判定する
+ * @param {number} num
+ * @return {boolean}
+ */
+function isMultipleOfSeventeen(num){
+    if (num%17 === 0) {return true}
+    return false
+};
 
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
