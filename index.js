cfe1f4dc180537c1a65f1c98d0936ade83a0b825
Iwasaki <iwasaki@arssystem.co.jp>
2018-04-16 08:20:45 +0000

    17で割り切れるか

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
+/**
+* 17で割り切れる
+* @param {number} number
+*/
+function isMultipleOfSeventeen(number) {
+    return number % 17 === 0;
+}
+
+
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
