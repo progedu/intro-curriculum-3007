60d333dd53d20f1f1365c1d6819701cbc45a422a
Hideaki Hori <hori.hide1003@gmail.com>
2018-04-26 04:18:45 +0000

    isMultipleOfSeventeen関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 'use strict';
 
-module.exports = {
+// 17で割り切れるものだけ取得する
+function isMultipleOfSeventeen(int) {
+    return int % 17 === 0;
 }
+
+module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
+};
