565500a3ab887a782c685b20c341aff5046ee291
MASAKI HORI <34409277+MASAKI-HORI@users.noreply.github.com>
2018-05-14 14:56:43 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 'use strict';
 
-module.exports = {
+function isMultipleOfSeventeen(value){
+        return value % 17 === 0;
 }
+
+module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
+};
