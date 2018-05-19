b06fe6ac55feefedb08d2322db837cb18a81c474
crsMtk <son_of_aki@mineo.jp>
2018-05-19 13:19:44 +0000

    isMultipleOfSeventeen

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 'use strict';
 
+/**
+* isMultipleOfSeventeenを追加する
+* @param {string} intValue
+* @return {boolean} 17で割り切れるか
+*/
+function isMultipleOfSeventeen(intValue) {
+    if(intValue % 17 === 0){
+        return true;
+    }　else {
+        return false;
+    }
+}
+
 module.exports = {
+    isMultipleOfSeventeen : isMultipleOfSeventeen
 }
