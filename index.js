07609193498819f77e529423f488e0796aa07adf
sasaki <copsti.3077@gmail.com>
2018-05-24 12:38:59 +0000

    seventeenモジュールにisMultipleOfSeventeen()関数を実装

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
+function isMultipleOfSeventeen(num){
+    if(num % 17 == 0){
+        return true;
+    } else {
+        return false;
+    }
+}
+
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
