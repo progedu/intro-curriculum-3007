04b98e4fb94f1c4c3d3c34e5dbbd4e506ecbd7e1
seiichi3141 <seiichi3141@gmail.com>
2018-05-28 10:55:44 +0000

    isMultipleOfSeventeenの実装

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
 
-module.exports = {
+function isMultipleOfSeventeen(num) {
+  if (num % 17) {
+    return false;
+  }
+  return true;
 }
+
+module.exports = {
+  isMultipleOfSeventeen: isMultipleOfSeventeen
+};
