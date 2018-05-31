8ba6efae8450f5d7971cdd6a69d09df00bd10ee5
k-o-j-i <akojima.rej@gmail.com>
2018-05-31 02:23:05 +0000

    isMultipleOfSeventeen 関数を実装

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
+/**
+ * 配列に含まれる整数が 17 で割り切れるものだけにする
+ * @param {number} num
+ */
+function isMultipleOfSeventeen(num) {
+    return num % 17 === 0;
+}
 
 module.exports = {
-}
+    isMultipleOfSeventeen: isMultipleOfSeventeen
+};
