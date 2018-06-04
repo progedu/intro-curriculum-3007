b972b7c0478560e10642f10f9b3e9667d6dd10e2
{bobchin} <kinjo.bobchin@gmail.com>
2018-06-05 02:02:55 +0900

    正解のプルリクです

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 'use strict';
 
+
+/**
+ * TODOを追加する
+ * @param {number} num
+ */
+
+function isMultipleOfSeventeen(num) {
+  return num % 17 === 0;
+}
+
 module.exports = {
+  isMultipleOfSeventeen: isMultipleOfSeventeen
 }
