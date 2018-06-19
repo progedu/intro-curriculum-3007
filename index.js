b0bbaefb678638dc2b4774f40c8aaf11da29f31f
Hiroaki Tagawa <h.tagawa@capandlow.com>
2018-06-19 08:25:27 +0000

    17の倍数かどうかを判定する関数を追加

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
-module.exports = {
+/**
+ * 17の倍数かどうかの判定
+ * @param {number} num 
+ */
+function isMultipleOfSeventeen(num) {
+  return (num % 17) === 0;
 }
+
+module.exports = {
+  isMultipleOfSeventeen: isMultipleOfSeventeen
+};
