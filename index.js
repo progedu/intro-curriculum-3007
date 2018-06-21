15097fac541dc8a031aad9728cb01a80861d6c3a
umaada <la.lune.nasu@gmail.com>
2018-06-21 08:04:39 +0000

    17の倍数か判別

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 'use strict';
 
+/**
+ * 整数が 17 で割り切れるかどうか
+ * @param {number} number
+ * @return {boolean} 整数が 17 で割り切れるかどうか
+ */
+function isMultipleOfSeventeen(number) {
+    if( number % 17 === 0)
+    {
+        return true;
+    }
+    return false;
+}
 module.exports = {
+    isMultipleOfSeventeen : isMultipleOfSeventeen
 }
