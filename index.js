397c87169d8b7e12dc66cb79e7ec6242abd0aae1
kakashiGit <kakashi.tea@gmail.com>
2018-06-24 03:57:07 +0000

    isMultipleOfSeventeenを実装

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 'use strict';
 
+function isMultipleOfSeventeen(num) {
+    // 17で割り切れるなら要素を吐き出す
+    return num % 17 === 0;
+    }
+
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
