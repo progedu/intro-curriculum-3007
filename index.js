66b56ac4d1e9a57185f0e62e9847b908ddbed324
axuceuje <40648964+axuceuje@users.noreply.github.com>
2018-07-06 10:23:38 +0900

    17で割り切れるかどうかの判定実装

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
+/**
+* 引数の値が 17 で割り切れるかどうかを判定する 
+* @param {number} num
+* @return {boolean} 17 で割り切れる場合 true
+*/
+function isMultipleOfSeventeen(num) {
+    return num % 17 === 0;
+}
+
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
