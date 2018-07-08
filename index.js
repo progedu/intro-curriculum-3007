3b308f07dfa1d53a7db6b95e06f2fc704bb587fb
noratora <4438333+noratora@users.noreply.github.com>
2018-07-08 06:29:59 +0000

    解答コードを実装

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
+/**
+ * 17で割り切れる整数かどうかを判定する
+ * @param {number} num
+ */
+function isMultipleOfSeventeen(num) {
+    return num % 17 === 0
+}
+
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
