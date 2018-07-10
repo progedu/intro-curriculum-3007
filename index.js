e107ce61711d25db9dda7ac1fec0f29193957032
carlitowiu <youllneverwalkalone0817@gmail.com>
2018-07-10 10:24:34 +0000

    特定の自動車メイカーかどうかを判定する関数とテストを実装しました

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ function isMultipleOfSeventeen(number){
     return number % 17 === 0;
 }
 
+const carMakers = ['Toyota','Nissan', 'BMW', 'Honda', 'GM', 'Ford'];
+
+function isCarMaker(company){
+    return carMakers.includes(company);
+}
+
 module.exports = {
-    isMultipleOfSeventeen: isMultipleOfSeventeen
+    isMultipleOfSeventeen: isMultipleOfSeventeen,
+    isCarMaker: isCarMaker
 }
