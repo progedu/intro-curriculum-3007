9b14e3636f34f95f891afd2840eae0d463b0a687
Kenji Miyata <miyatakenji0922@gmail.com>
2018-07-10 12:15:01 +0000

    17で割り切れるかの判定

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 'use strict';
 
+/**
+ * 17で割り切れるかの判定
+ * @param num
+ */
+function isMultipleOfSeventeen (num) {
+    if (num % 17 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
 module.exports = {
+    isMultipleOfSeventeen : isMultipleOfSeventeen
 }
