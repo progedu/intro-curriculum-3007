e07585be5a5af2e6ad9319cd818fadfcd9c154a4
uekichi <41245953+uekichi@users.noreply.github.com>
2018-07-26 08:56:32 +0900

    17で割り切れる判定

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
+/**
+ * 17で割り切れるかを判定する関数isMultipleOfSeventeen
+ */
+function isMultipleOfSeventeen(num) {
+    if (num % 17 === 0) {
+        return true
+    }
+}
+
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
