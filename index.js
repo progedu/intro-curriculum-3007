2243e1fb2abd46b519776c4265503b0f1acc458e
takeshi <jesuisneko@gmail.com>
2018-07-26 15:07:35 +0000

    seventeenモジュールにisMultipleOfSeventeen関数の実装完了

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 'use strict';
-
-module.exports = {
+/**
+ * 17の倍数のとき true を返す
+ * @param {number} num 
+ */
+function isMultipleOfSeventeen(number) {
+    return number % 17 === 0;
 }
+module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
+};
