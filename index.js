83ab1e1d68a6dd3f84294f40f32ad1c9a06efa70
uekichi <41245953+uekichi@users.noreply.github.com>
2018-07-26 22:18:08 +0900

    練習問題

--- index.js
+++ index.js
@@ -4,9 +4,7 @@
  * 17で割り切れるかを判定する関数isMultipleOfSeventeen
  */
 function isMultipleOfSeventeen(num) {
-    if (num % 17 === 0) {
-        return true
-    }
+    if num % 17 === 0;
 }
 
 module.exports = {
