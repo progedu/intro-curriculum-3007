aa47d877bde0a98386d46aa577d6ecc827234da6
k-zin <katesuzuki0113@gmail.com>
2018-07-31 02:21:59 +0000

    テストに合わせ関数を実装

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
+/**
+ * 17で割り切れる場合trueを返す
+ * @param {Int} num
+ * @return boolean
+ */
+function isMultipleOfSeventeen(num) {
+    return num % 17 === 0;
+}
+
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
