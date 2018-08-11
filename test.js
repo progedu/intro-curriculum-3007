38358e1a2cab3a322328dc32328a853fd08d3edb
Soichiro Yoshimura <yoshimura@soichiro.org>
2018-08-12 02:17:23 +0900

    Update test.js

--- test.js
+++ test.js
@@ -6,3 +6,5 @@ const a = [83, 32, 85, 47, 77, 8, 61, 74, 29, 34, 11, 76, 60, 99, 55, 7, 19, 60,
 assert.deepEqual(a.filter(seventeen.isMultipleOfSeventeen), [85, 34]);
 
 console.log('テストが正常に完了しました');
+// プルリクテスト
+
