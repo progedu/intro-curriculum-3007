5b0f01911698738e4f2bba9c81fbcbd51590191f
capybaradesuga <capybaradesuga@gmail.com>
2018-08-11 02:30:04 +0000

    isMultipleOfSeventeen 関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 'use strict';
 
+/**
+ * 整数が17で割り切れるかどうか判定する
+ * @param {num} integer
+ * @return {boolean} boolean 
+ * ↓
+ * 答え
+ * 17の倍数である場合に true を返す
+ * @param {number} num
+ */
+function isMultipleOfSeventeen(num) {
+    return num % 17 === 0;
+}
+
+
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
