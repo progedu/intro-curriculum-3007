52fed99d69eb52c5eb70a8e22aa1cadf99738fdc
Kazuki Hakozaki <kb240225@gmail.com>
2018-08-12 00:54:44 +0000

    [add] 17で割り切れるかどうかの関数の追加

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
+/**
+* 配列に含まれる整数が17で割り切れるかどうか
+* @param {number} num
+*/
+function isMultipleOfSeventeen(num) {
+  return num % 17 === 0;
+}
 
 module.exports = {
+  isMultipleOfSeventeen : isMultipleOfSeventeen
 }
