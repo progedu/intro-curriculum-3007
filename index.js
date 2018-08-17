52eb6090749560fd787e5b32f498857ea8322c62
LinuRina <aoiz0805@yahoo.co.jp>
2018-08-17 11:03:43 +0000

    seventeenモジュールの実装

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
+/**
+ * 
+ * @param {number} n 
+ */
+function isMultipleOfSeventeen(n){
+    return n % 17 === 0;
+}
+
+
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
