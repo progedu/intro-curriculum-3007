92db0b79b002359da3b60ee36d95e1067d78260c
daichi-kaeru <daichi_18n1102087@nnn.ed.jp>
2018-08-23 20:56:57 +0900

    isMultipleOfSeventeen関数を実装

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 'use strict';
 
+
+/**
+ * 整数が 17 で割り切れるかどうかを返す
+ * @param {number} num
+ */
+function isMultipleOfSeventeen(num) {
+    return num % 17 === 0;
+}
+
+
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
