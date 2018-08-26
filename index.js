ddaab01866ff5341c673f49720c8d06846d9d262
x122425x <x122425x@gmail.com>
2018-08-26 09:39:29 +0000

    isMultipleOfSeventeen関数の追加

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
-module.exports = {
+/**
+ * 配列を取得し、17で割り切れる数字のみの配列を作成
+ * @param {number} num 
+ */
+function isMultipleOfSeventeen(num) {
+    return num%17===0;
 }
+
+module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
+};
