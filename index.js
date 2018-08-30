394fc5dd773302842d654d86d6e11a2d501725c4
xukream <nekomeshi0526@softbank.ne.jp>
2018-08-30 20:05:32 +0000

    isMultipleOfSeventeen関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
+/**
+ * 整数が17で割り切れるときtrueを返す
+ * @param {number} num
+ * @return {boolean}
+ */
+function isMultipleOfSeventeen(num) {
+    return num % 17 === 0;
+}
 
 module.exports = {
-}
+    isMultipleOfSeventeen: isMultipleOfSeventeen
+};
