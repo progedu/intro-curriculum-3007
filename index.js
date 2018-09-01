b0b9db3d4d0cdcf822f7504891bd3e20c029744e
EkeMinusYou <ekeminusyou@gmail.com>
2018-09-01 16:00:44 +0000

    17で割り切れるか確認するモジュールを追加

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
+/**
+ * 17で割り切れるかを返す
+ * @param {int} tgt
+ */
+function isMultipleOfSeventeen(tgt) {
+    return tgt % 17 === 0
+}
+
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
