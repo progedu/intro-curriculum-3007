0a745f33ce82595e7ad63d2c2c0bcc2813e1959a
kaputte <kaputte@gmail.com>
2018-09-05 21:27:13 +0000

    Add isMultipleOfSeventeen

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
+/**
+ * 配列に含まれる整数が17が割り切れるとき true を返す
+ * @param (number) num
+ */
+function isMultipleOfSeventeen (num){
+    return num % 17 === 0;
+}
+
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
