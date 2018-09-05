51149c0bfc308eb85898c140923c237b777463dc
kaputte <kaputte@gmail.com>
2018-09-05 21:32:17 +0000

    コメント記載方法が誤っていたため修正

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 
 /**
- * 配列に含まれる整数が17が割り切れるとき true を返す
- * @param (number) num
+ * 引数が17が割り切れるとき true を返す
+ * @param {number} num
  */
 function isMultipleOfSeventeen (num){
     return num % 17 === 0;
