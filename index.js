722c70f37fdafcb3f557e8686dce7204a78815f8
miifriends <miikun114514@gmail.com>
2018-09-12 01:36:08 +0000

    17 で割り切れるかどうかを判定する関数を実装

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
-module.exports = {
+/**
+ * 17 で割り切れるかどうかを返す
+ * @param {number} num - 数字
+ */
+function isMultipleOfSeventeen(num) {
+  return num % 17 === 0;
 }
+
+module.exports = {
+  isMultipleOfSeventeen
+};
