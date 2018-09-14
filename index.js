ea1cc7a02a2719965af6d7239c7dbc0720c8f865
sakaime <sakaime2@gmail.com>
2018-09-14 20:29:29 +0000

    Solved

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 'use strict';
 
+function isMultipleOfSeventeen(integer) {
+    if (integer % 17 === 0) {
+        return true;
+    }
+}
+
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
