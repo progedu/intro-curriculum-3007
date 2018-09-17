d0f89e58c0848274ce33641f134bdab52f7e0825
jun110 <subaddress.jk110@gmail.com>
2018-09-17 12:36:49 +0000

    モジュール化された処理の演習問題をコミット

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
 
+function isMultipleOfSeventeen(value){
+    if( 0 === value % 17 ){
+        return true
+    }
+    return false
+}
+
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
