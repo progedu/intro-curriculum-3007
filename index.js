d5f9f47ee06f089e0deb1aafc57575a8ffa43b9e
Julien-Watanabe <masafumi303@gmail.com>
2018-09-20 05:13:10 +0000

    内容変更

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 'use strict';
 
-module.exports = {
+//配列に含まれる整数が17で割り切れるものだけにするモジュールを実装
+
+/**
+ * 整数が17の倍数か判定する関数
+ * @param {number} num
+ */
+function isMultipleOfSeventeen(num) {
+    return num % 17 === 0;
 }
+
+module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
+};
