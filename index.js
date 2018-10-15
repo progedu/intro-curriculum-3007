fc9731e8b40ad9ad0c01f847af2195bdff65d2fd
karyuse <kakubari@g-plan.net>
2018-10-15 05:10:54 +0000

    関数isMultipleOfSeventeenを実装

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
-module.exports = {
+/**
+ * 整数が17で割り切れるかどうか判定する
+ * @param {number} num
+ * return {boolean] 割り切れたかどうか
+**/
+function isMultipleOfSeventeen (num) {
+	return num % 17 === 0;
 }
+
+module.exports = {
+	isMultipleOfSeventeen: isMultipleOfSeventeen
+};
