7bba58b0a365d3b20cb580f55df7d906c597834a
heha-proglea <heha965+proglea@gmail.com>
2018-10-19 14:35:50 +0000

    update: isMultipleOfSeventeen関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 'use strict';
 
-module.exports = {
+/**
+ * 17の倍数である場合にtrueを返す ≡ 0 (mod17)
+ * @param {number} num
+ * @return {boolean} 
+ */
+
+function isMultipleOfSeventeen(num) {
+    return num % 17 === 0;
 }
+
+module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
+};
