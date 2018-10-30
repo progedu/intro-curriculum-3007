0677c55f0b22856ebebbfeabb9de329ed20cc82b
kansuke224 <kansuke224224@gmail.com>
2018-10-30 22:24:17 +0000

    isMultipleOfSeventeen 関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
-module.exports = {
+/**
+* 配列に含まれる整数が17で割り切れるかどうか判定する
+* @param {number} numbers
+*/
+function isMultipleOfSeventeen(numbers) {
+  return numbers % 17 === 0;
 }
+
+module.exports = {
+  isMultipleOfSeventeen
+};
