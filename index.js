6bb36a7e83de2fa2d44497899b70e6e793a955f6
sdk40010 <sdk40010@gmail.com>
2018-11-04 13:36:36 +0000

    17の倍数どうかの判定

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
+/**
+ * 17の倍数か判定する
+ * @param {number} integer  
+ */
+function isMultipleOfSeventeen(integer) {
+    return integer % 17 === 0;
+}
+
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
