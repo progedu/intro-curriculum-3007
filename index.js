103699cddabe1377dd2a8c7990971259f9a0c0f3
carefreecat <t.saitou.666@gmail.com>
2018-11-14 17:33:58 +0000

    整数が 17 で割り切れるかどうかを判定

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+
 'use strict';
 
+/** 
+* 整数が 17 で割り切れるかどうかを判定
+* @param {int} num
+* 
+* @return {boolean}
+*/
+function isMultipleOfSeventeen(num){
+    return num % 17 === 0;
+
+}
+
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
