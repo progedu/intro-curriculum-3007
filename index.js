24afa66ac5d906ed7ffab9d013aa0cbc39bc51c5
pm27-27 <pinehill2727@gmail.com>
2018-11-18 08:10:02 +0000

    seventeen モジュールの実装

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
 'use strict';
 
+/**
+ * 17で割り切れるかどうか判定する
+ * @param {int} n
+ * @return {boolean}
+ */
+function isMultipleOfSeventeen(n) {
+    if (n % 17 === 0){
+        return true;
+    };
+}
+
+/**
+ * 配列に含まれている整数を17で割り切れるものだけにする
+ * @param {array} numlist
+ * @return {array} 
+ */
+function seventeen(numlist) {
+    return numlist.filter(isMultipleOfSeventeen);
+}
+
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen,
+    seventeen: seventeen
 }
