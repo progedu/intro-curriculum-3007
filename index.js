87acb0a6acaf7af436a6ab3044413c18ef8bde7e
pirorin91 <pirorin91@gmail.com>
2018-11-25 01:40:22 +0900

    Implement isMultipleOfSeventeen

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
+/**
+* inputが17の倍数ならtrueを返す
+* @param {number} input
+*/
+function isMultipleOfSeventeen(input) {
+    return input % 17 === 0;
+}
+
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen 
 }
