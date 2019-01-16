ee101cf729a1869ed284641f7777808bb094ad50
rnatsurno <rnatsurno420@gmail.com>
2019-01-16 21:16:22 +0900

    Update: isMultipleOfSeventeen関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 'use strict';
+/**
+* 17の倍数である場合 true を返す
+* @param {number} num
+*/
+const isMultipleOfSeventeen = n => n % 17 === 0;
 
 module.exports = {
+  isMultipleOfSeventeen: isMultipleOfSeventeen
 }
