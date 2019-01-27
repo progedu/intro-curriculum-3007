77bd2d1c52fcbfc7f03f21f2a65230ae20e22df7
miharu <nars024@gmail.com>
2019-01-27 23:20:13 +0000

    add a function isMultipleOfSeventeen

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
+/**
+ * 整数が17で割り切れるかどうかを判定する
+ * @param {number} number 
+ */
+function isMultipleOfSeventeen(number) {
+    return number % 17 === 0;
+}
+
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
