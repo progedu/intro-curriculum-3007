f8103df8b1bcec3ffc26c76d76cab5d732940cc3
Taiyo Utsuhara <33287507+taiyoutsuhara@users.noreply.github.com>
2019-03-02 14:38:55 +0000

    17|nの実装

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 'use strict';
-
-module.exports = {
+/**
+* 配列に含まれる整数が17で割り切れるものだけにする。
+* @param {number} integer
+*/
+function isMultipleOfSeventeen (integer){
+    return integer % 17 == 0;
 }
+module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
+};
