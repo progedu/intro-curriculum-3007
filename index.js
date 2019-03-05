5b5559346b52263b4071abbdd1e03826e2255de2
shorie <s.horie@rainbow.co.jp>
2019-03-05 09:53:18 +0000

    isMultipleOfSeventeen 関数を実装

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
+/**
+* 17の倍数をフィルタリング
+* @param {number} n
+*/
+function isMultipleOfSeventeen (n) {
+    return n%17 == 0;
+}
+
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
