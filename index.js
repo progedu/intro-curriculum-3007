e85df3c3f4d6ee8e59544398279a7fa0210e874d
Melon0306 <t.kily0121@gmail.com>
2019-03-05 18:01:30 +0000

    17 で割り切れるものだけにする seventeen モジュールを実装

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
+/**
+ * 17で割り切れるか
+ * @param {number} number 
+ */
 
+function isMultipleOfSeventeen(number){
+    return number % 17 === 0;
+}
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
