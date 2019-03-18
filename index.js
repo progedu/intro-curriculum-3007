62763e4addb64526ac3b5d6d8cc62fb21641bc52
noskwnosky <noskwnosky@gmail.com>
2019-03-18 11:08:39 +0000

    17で割り切れるか判定する関数

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
+/**
+* 17で割り切れるかどうかを判定する
+* @param {var} number
+*/
+function isMultipleOfSeventeen(number) {
+    return number % 17 === 0;
+}
+
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
