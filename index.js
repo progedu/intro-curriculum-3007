d3f91bd56b2404ccda01bd47f8c0f359b6956aeb
ragdoll2019 <gliffin93@gmail.com>
2019-03-30 11:48:09 +0000

    整数が17で割り切れるかどうかを判定する isMultipleOfSeventeen 関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
+/**
+ * 17で割り切れる数を返す
+ * @param {number} num 
+ */
+function isMultipleOfSeventeen(num) {
+  return num % 17 === 0;
+}
+
 module.exports = {
+  isMultipleOfSeventeen: isMultipleOfSeventeen
 }
