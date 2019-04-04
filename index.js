8311e59148d7d648b9d7b43dc2257d0841b75011
inosato <si17_21@yahoo.co.jp>
2019-04-04 16:06:15 +0000

    17で割り切れるかどうか判断する関数を追加

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-'use strict';
+"use strict";
+
+/**
+ * 17で割り切れるかの真偽値を返却する
+ * @param {Number} num
+ * @return {boolean}
+ */
+function isMultipleOfSeventeen(num) {
+  return num % 17 === 0;
+}
 
 module.exports = {
-}
+  isMultipleOfSeventeen: isMultipleOfSeventeen
+};
