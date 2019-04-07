9c17adaab8188945924e8324f049a40cc5cb7d8e
HjAkeru <window.akeru@gmail.com>
2019-04-07 02:20:56 +0000

    17で割り切れる数か判定する機能を実装

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
+/**
+ * 17で割り切れる数かどうか
+ * @param {number} num 
+ * @return {boolean}
+ */
+function isMultipleOfSeventeen(num){
+    return num % 17 === 0;
+}
+
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
