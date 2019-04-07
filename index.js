878e97fd6a2a78b7fd00ebf1ef9a88202964ddb6
eijiha <keijih1986@gmail.com>
2019-04-07 01:31:43 +0000

    3007の演習問題の回答

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 'use strict';
 
+/**
+ * 配列に含まれる整数を１７で割り切れるかどうか判定する
+ */
+function isMultipleOfSeventeen(number) {
+    return (number % 17) === 0
+}
 module.exports = {
+    isMultipleOfSeventeen
 }
