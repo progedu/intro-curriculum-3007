2a20a078e1fcc7669b11071d2089c31b3a496f1b
Kentarou Asai <asaknkn@gmail.com>
2019-04-21 05:08:42 +0000

    整数が 17 で割り切れるかどうかを判定するモジュール

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
+/**
+ * 整数が 17 で割り切れるかどうかを判定
+ * @param {number} num
+ * @return num 17で割り切れる数
+ */
+function isMultipleOfSeventeen(num) {
+  return num % 17 === 0;
+}
 
 module.exports = {
+  isMultipleOfSeventeen
 }
