00321e4fead1c1d6aadd84e7494235d0911e848e
nemuri90 <while.flag.equal.one@gmail.com>
2019-05-04 23:53:43 +0900

    isMultipleOfSeventeen関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
+/**
+ * is number multiple of 17
+ * @param {number} number
+ * @returns {boolean}  
+ */
+function isMultipleOfSeventeen(number) {
+    return (number % 17) === 0;
+}
+
 module.exports = {
+    isMultipleOfSeventeen
 }
