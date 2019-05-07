e9ec05e435cbaa7eb1863b35abc65d63b297ef32
joeihata <j_ihata@uluru.jp>
2019-05-07 09:40:34 +0000

    this is a study program

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
+/**
+ * if it is the number that can be divisible, return true
+ * @param {number} rum
+ */
+function isMultipleOfSeventeen(num) {
+    return num % 17 === 0;
+}
 
 module.exports = {
-}
+    isMultipleOfSeventeen: isMultipleOfSeventeen
+};
