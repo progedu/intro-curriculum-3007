de717926f9f2f1f462c8dacd40ed4a1150a30e0e
kouichi chikata <kouichi.ckt@gmail.com>
2019-05-21 00:43:02 +0900

    isMultipleOfSeventeen 関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 'use strict';
 
-module.exports = {
-}
+/**
+ * 配列に含まれる整数が17の倍数であるかを判定する
+ * @param {number} number
+ * @return {boolean}
+ */
+function isMultipleOfSeventeen(number) {
+    return number % 17 === 0;
+} 
+module.exports = { isMultipleOfSeventeen }
