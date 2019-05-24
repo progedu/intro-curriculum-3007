00b03d40a47d879b35d8b51c6b37bf21c72f1cbd
Yoshifumi Matsui <cbk12360@gmail.com>
2019-05-24 22:42:12 +1200

    seventeen モジュールの実装

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 'use strict';
 
+/**
+* 整数の配列を受け取り、17で割り切れるかを返す
+* @param {array} num
+* @return {boolean} 17で割り切れるかどうか
+*/
+function isMultipleOfSeventeen(num) {
+    if (num % 17 === 0){
+        return true;
+    } else {
+        return false;
+    }
+}
+
 module.exports = {
+    isMultipleOfSeventeen
 }
