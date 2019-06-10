2b430e9e50998775e0fbe4cd63fe68e23ef06ea3
Tsukino Furukawa <tsukino95@gmail.com>
2019-06-10 14:43:30 +0000

    implement isMultipleOfSeventeen

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
+/**
+ * 整数を受け取って17で割り切れるかどうかを返す
+ * @param {number} num
+ * @return {boolean}
+ */
+function isMultipleOfSeventeen(num) {
+    return num % 17 === 0
+}
+
 module.exports = {
+    isMultipleOfSeventeen
 }
