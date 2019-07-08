88227ad32dd7b522ec03e35ac2960e5395e8d186
Yuta <l0tus.yang.dage@gmail.com>
2019-07-08 22:26:07 +0900

    submit

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
 
+function isMultipleOfSeventeen(x) {
+  return x % 17 === 0
+}
+
+
+const a = [83, 32, 85, 47, 77, 8, 61, 74, 29, 34, 11, 76, 60, 99, 55, 7, 19, 60, 98, 38, 28, 96, 32];
+console.log(a.filter(isMultipleOfSeventeen))
 module.exports = {
+  isMultipleOfSeventeen
 }
