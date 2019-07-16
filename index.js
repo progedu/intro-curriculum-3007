1d0aaf48c87b1d69ae8629e390288e35a2aecfde
naoya <naoya.hikida@hpe.com>
2019-07-16 10:21:47 +0000

    17の倍数

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 'use strict';
 
-module.exports = {
+/**
+ * 配列に含まれる整数が17で割り切れるか判断
+ * @param {string} task
+ */
+
+function isMultipleOfSeventeen(task){
+    if(task%17==0){
+        return true;
+    }
+    return false;
 }
+
+module.exports = {
+    isMultipleOfSeventeen
+};
