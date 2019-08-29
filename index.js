60221cd517ec06651057bb10d90f107b7327bcd2
Haruna Karasawa <haruna_karasawa@nnn.ac.jp>
2019-08-29 16:56:58 +0900

    isMultipleOfSeventeen関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 'use strict';
-
+/**
+ * 配列に含まれる整数が17で割り切れる条件式を返す
+ * @return {string} 
+ */
+var isMultipleOfSeventeen = (number) => { 
+    return number % 17 == 0;
+}
 module.exports = {
+    isMultipleOfSeventeen
 }
