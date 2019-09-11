9f7afc341df0629a02056a03efe50b57de118823
igahide128 <igahide128@gmail.com>
2019-09-11 19:57:35 +0900

    isMultipleOfSeventeen関数のコメント部修正

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 'use strict';
 /**
  * 17 で割り切れるかどうかを返す関数
- * @param {integer} val
+ * @param {number} num
  * @return {boolean} 17で割り切れるかどうか
 */
-function isMultipleOfSeventeen(val) {
-  return val % 17 === 0 ;
+function isMultipleOfSeventeen(num) {
+  return num % 17 === 0 ;
 }
 
 module.exports = {
