b17a0abee7e76b34687eb161c682b159aa4a30f9
katana2019 <1031yoshi@jcom.zaq.ne.jp>
2019-09-14 18:13:52 +0900

    3-7 練習問題の回答

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 'use strict';
-
-module.exports = {
+/**
+ * 引数が17の倍数の時、ture を返す
+ * @param {number} n 
+ */
+function isMultipleOfSeventeen(n) {
+  if (n < 0) {return };
+  return n % 17 === 0;
 }
+module.exports = { isMultipleOfSeventeen };
