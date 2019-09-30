2560dd8528a604ad14a7a854e09348749c0e40ac
AIOI <aioi18223@yahoo.co.jp>
2019-09-30 19:22:23 +0900

    　関数追加

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 'use strict';
 
-module.exports = {
+
+
+/**
+ * 17の倍数だったらtrueを返す
+ * @param {number} num
+ */
+function isMultipleOfSeventeen(num){
+    return num % 17 === 0
 }
+
+module.exports = {
+    isMultipleOfSeventeen
+};
