39a5c46d2b743f71c4ba52e50e726c1b1b9f9624
takayan40 <f15bc010@gmail.com>
2019-10-07 02:44:00 +0000

    seventeen モジュールの追加

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
+/**
+ * 
+ * １７の倍数である場合 true を返す
+ * @param {number} n 
+ */
+function isMultipleOfSeventeen(n){
+    return n % 17 === 0;
+}
+
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
