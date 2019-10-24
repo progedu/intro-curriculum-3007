ea70b291967f5af27299505e6627eadcefb397c5
Hatsunori Kashima <hatsunri.kashima@gmail.com>
2019-10-24 21:41:36 +0900

    isMultipleOfSeventeen関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
+/**
+ * 整数が17で割り切れるかどうかを判定する
+ * @param {int} num
+ * @return {boolean} 17で割り切れるかどうか
+ */
+function isMultipleOfSeventeen(num) {
+    return num % 17 === 0;
+}
+
 module.exports = {
+    isMultipleOfSeventeen
 }
