05e25f1f48d0fcc97afecccc97ade4937d2e25fc
kazuya-hub <56819991+kazuya-hub@users.noreply.github.com>
2019-10-24 15:17:40 +0900

    isMultipleOfSeventeen関数を実装しました

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
+/**
+ * @param {Numner} number
+ * @return {Boolean} numberが17で割り切れるかどうか
+ */
+isMultipleOfSeventeen(number) {
+      return (number % 17) === 0;
+}
+
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
