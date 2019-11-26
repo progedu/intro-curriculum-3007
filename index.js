272e733b288d5575b0f2be8e8d2dcbff4cc7d591
Hiroki Motooka <hiroki161@gmail.com>
2019-11-27 02:10:33 +0900

    A function finds multiple of 17

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
-module.exports = {
+/**
+ * 17で割り切れる整数にTrueを返す。
+ * @return {array} nums
+ */
+function isMultipleOfSeventeen(nums) {
+  return nums % 17 === 0;
 }
+
+module.exports = {
+    isMultipleOfSeventeen
+};
