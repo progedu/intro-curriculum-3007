5282df4a190fafc3d02eef8f4998ff782d997b8d
JunIshino <jun.ishino@kepple.jp>
2019-12-30 15:36:51 +0900

    isMultipleOfSeventeen 関数を実装

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
 
-module.exports = {
+/**
+* 17の倍数である場合 true を返す
+* @param {number} num
+* @return {boolean} 17で割れたかどうか
+*/
+function isMultipleOfSeventeen(number) {
+  return number % 17 === 0;
 }
+
+module.exports = {isMultipleOfSeventeen}
