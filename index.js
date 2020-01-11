312473baf93ead0c4cf738cf72a85e20545b6401
Tee <milk06pop.12popken@gmail.com>
2020-01-11 12:34:55 +0900

    関数の定義

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
+function  isMultipleOfSeventeen(num){　//関数の定義
+    if(num % 17 === 0){ //あまりが0ならtrueを返す
+        return true;
+    }else {　//違うならfalse
+    return false;
+    }
 
+}
 module.exports = {
+    isMultipleOfSeventeen
 }
