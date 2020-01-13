41d707565393855d7c8175e64a4790adf725a284
stellar-mouse <s_mouse@amigo2.ne.jp>
2020-01-13 11:47:17 +0900

    isMultipleOfSeventeen をモジュールに実装

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
+/**
+ * 17 で割り切れる場合 true を返す
+ * @param {number} value
+ * @return {boolean}
+ */
+function isMultipleOfSeventeen(value){
+    return value % 17 === 0;
+}
 
 module.exports = {
+    isMultipleOfSeventeen
 }
