034cce9fa0f6e46fae24725d509ece945ea36eb8
Kazuma Murata <kazumario123@gmail.com>
2020-01-15 23:18:35 +0900

    isMultipleOfSeventeen関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
+/**
+ * 値が17で割り切れるときtureを返す
+ * @param {number} num 
+ */
+
+function isMultipleOfSeventeen(num) {
+  return num % 17 === 0;
+}
+
 module.exports = {
+  isMultipleOfSeventeen
 }
