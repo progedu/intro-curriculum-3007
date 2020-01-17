1711f68fb399add669d32a4438fd08160456fd1d
hayatology <hayathology@icloud.com>
2020-01-17 22:07:31 +0900

    課題

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
+/**
+* 17で割り切れる数のみを返す
+* @param {number} number
+* @return {boolean} 真偽
+*/
+function isMultipleOfSeventeen(number) {
+    return number % 17 === 0;
+}
 
 module.exports = {
-}
+    isMultipleOfSeventeen
+};
