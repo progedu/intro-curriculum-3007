d9765c07faaf51ed2538fec44a35f36b5c7a076a
Shingo Takoh <animuberi@gmail.com>
2020-01-21 14:32:54 +0900

    練習問題のプルリク

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 'use strict';
-
-module.exports = {
+/**
+ * 整数が 17 で割り切れるかどうかを判定する
+ * @param {number} num
+ */
+function isMultipleOfSeventeen(num) {
+    return num % 17 === 0;
 }
+module.exports = {
+    isMultipleOfSeventeen
+};
