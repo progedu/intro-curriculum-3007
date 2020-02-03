35577b66ddfed5a8b197ad2323ba395b2126b971
shirokuroneko-x <work9adjust9atm@gmail.com>
2020-02-03 23:08:36 +0900

    isMultipleOfSeventeen関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
-
+/**
+ * 17で割り切れる
+ * @params {integer} num
+ * @return {boolean}
+ */
+function isMultipleOfSeventeen(num){
+    return num % 17 === 0;
+}
 module.exports = {
+    isMultipleOfSeventeen
 }
