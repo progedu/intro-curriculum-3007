3abf4e3a2d150092a71187bf8c1fe122be374ae9
wayadahata <gakky.taihasoeru@gmail.com>
2020-02-07 19:44:47 +0900

    index.jsを更新

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
+/**
+ * 17の倍数である true を返す
+ * @param {number}
+ */
+function isMultipleOfSeventeen(num) {
+  return num % 17 === 0;
+}
+
 module.exports = {
+  isMultipleOfSeventeen
 }
