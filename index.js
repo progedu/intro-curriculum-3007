bf75fdc4020285b66c5cf31bfe77862bf046ab54
awyaki <awyakitaka@gmail.com>
2020-02-07 07:47:08 +0000

    isMultipleOfSeventeen function was deployed

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
+//整数が17で割り切れるか判定する
+//@param { integer } 
+//@return { boolean } 割り切れるときtrueを返す
+let isMultipleOfSeventeen = (number) => {
+    return number%17 === 0;
+};
+
+
 module.exports = {
+    isMultipleOfSeventeen
 }
