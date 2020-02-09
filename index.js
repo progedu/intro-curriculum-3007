b3837fd1fd1a1bd8eeea42e25db89cbe006342a1
lvflwr <mayhem123757@gmail.com>
2020-02-09 17:56:30 +0000

    isMultipleOfSeventeen関数を実装

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 'use strict';
 
-module.exports = {
+/**
+ * 整数が17の倍数であるかどうかを判別する
+ * @param {int} number
+ * @return {bool}
+ */
+function isMultipleOfSeventeen(number) {
+    if (number %17 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+module.exports = { isMultipleOfSeventeen
 }
