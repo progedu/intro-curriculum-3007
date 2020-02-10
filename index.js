2451fd8afc12063b26181c13fa031d1e4cddbcc8
NOBUAKI KUROSAWA <nobuaki5714@gmail.com>
2020-02-11 01:15:45 +0900

    17で割り切れる数を選択する関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 'use strict';
-
-module.exports = {
+/**
+ * 17で割り切れる数を計算する
+ * @param {string} num 
+ */
+function isMultipleOfSeventeen(num) {
+    return num % 17 === 0;
 }
+module.exports = {
+    isMultipleOfSeventeen
+};
