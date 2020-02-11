df2b8fec257b7fa3d7ca788d5c70b87faf2b607f
tpky31092 <tpky31092@gmail.com>
2020-02-11 23:27:32 +0900

    isMultipleOfSeventeen関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 'use strict';
 
-module.exports = {
+/**
+* 数値を受け取る
+* @param {array} nums
+* @return {boolean} 割り切れるか否か
+*/
+function isMultipleOfSeventeen(nums) {
+    let result = nums % 17;
+    if (result === 0) {
+        return true;
+    } 
+        return false;
 }
+
+module.exports = {
+    isMultipleOfSeventeen
+};
