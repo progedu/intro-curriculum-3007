299a354c7bd3fb3d173754601c609587c1dc4c45
kensyo <takitateokowa@gmail.com>
2020-02-14 11:44:27 +0900

    17で割り切れるかどうか判定する関数を追加

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 'use strict';
 
+/**
+ * @param {number} num 整数
+ * @return {boolean} 真偽
+ */
+function isMultipleOfSeventeen(num) {
+    if (num % 17 === 0) {
+        return true;
+    }
+    return false;
+}
+
 module.exports = {
+    isMultipleOfSeventeen
 }
