31d3a065dcf7da71f1a9e10e710c2dc8507a3ae1
Tetsuya Sakai <t.sakai.1972@gmail.com>
2020-02-28 15:03:28 +0900

    17実装

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 'use strict';
-
+/**
+ * 17の倍数である場合trueを返す
+ * @pram {number} num
+ */
+function isMultpleOfSeventeen(num) {
+    return num % 17 === 0;
+}
 module.exports = {
+    isMultpleOfSeventeen
 }
