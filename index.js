f81142f54c0af673a14495632a9659b5c27ad29a
Naoto Ono <nono19@students.desu.edu>
2020-03-01 12:46:04 +0900

    isMultipleOfSeventeen関数をseventeenモジュールに実装

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
+/**
+ * 17で割り切れるとtrueを返す
+ * @param {number} num 
+ */
+
+function isMultipleOfSeventeen(num) {
+  return num % 17 === 0
+}
+
 module.exports = {
+  isMultipleOfSeventeen
 }
