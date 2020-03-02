61a13b149a5d7121a450afce1a848356d66e78bf
eolpva <e4jcbdv3z7@gmail.com>
2020-03-03 01:08:03 +0900

    関数追加

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 'use strict';
-
+/**
+ * 17の倍数の場合に true を返す
+ * @param {number} num 
+ */
+function isMultipleOfSeventeen(num) {
+    return num % 17 === 0;
+}
 module.exports = {
+    isMultipleOfSeventeen
 }
