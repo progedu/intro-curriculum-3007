4cf139657089b76abeaf3938a7c97992527b6033
taku1010 <takumi.guitar1010@gmail.com>
2020-03-05 17:05:12 +0900

    17で割り切れる数

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
-module.exports = {
+/**
+* TODOを完了状態にする
+* @param {array} Seventeen
+*/
+function isMultipleOfSeventeen(Seventeen){
+    return Seventeen % 17 === 0;
 }
+
+module.exports = {
+    isMultipleOfSeventeen
+};
