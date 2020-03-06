c40d4232ebb199f13044f15d0065ee7ce2ac8245
ttmone <tomone.ameno@gmail.com>
2020-03-06 20:43:27 +0900

    練習課題の実装

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
+/**
+ * 17で割り切れるかどうか判定する
+ * @param {integer} 整数
+ * @return {boolean}
+ */
+function isMultipleOfSeventeen(num) {
+  return num % 17 === 0;
+}
 
 module.exports = {
+  isMultipleOfSeventeen,
 }
