47bfe8ec1bc27ff5b60c21a3ddc07c67505b508a
sken <ken.s.rainbow@gmail.com>
2020-03-07 21:22:10 +0000

    練習問題3007

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
 
+/**
+ * check if the number is multiple of seventeen
+ * @param{num}
+ */
+function isMultipleOfSeventeen(num) {
+    return num % 17 === 0;
+}
 module.exports = {
+    isMultipleOfSeventeen
 }
