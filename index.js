b0bb9aef2634e8536e8c02e5ed113353f06cbd6e
sous <sous.hrk@gmail.com>
2020-03-07 16:57:18 +0900

    isMultipleOfSeventeen関数を追加

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
-module.exports = {
+/**
+ * 配列の中身を17で割り切れるものだけにする
+ * @param {array} num 
+ */
+function isMultipleOfSeventeen(num){
+    return num % 17 === 0;
 }
+
+module.exports = {
+    isMultipleOfSeventeen
+};
