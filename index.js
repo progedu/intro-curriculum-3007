2956205a54e97b0999eca68ee9d99118a655672a
arrow2nd <arrowsub57@gmail.com>
2020-03-08 10:14:29 +0000

    isMultipleOfSeventeen関数を実装しました。

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 'use strict';
 
-module.exports = {
+/**
+ * 17で割れる場合、trueを返す
+ * @param {number} n 
+ */
+function isMultipleOfSeventeen(n) {
+    return n % 17 === 0;
 }
+
+module.exports = { isMultipleOfSeventeen };
