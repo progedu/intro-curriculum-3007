b0cbf4e27ba8ed479c7f20e2c5e3d5f4a7f08a9f
stacru <hamaw8888@gmail.com>
2020-03-12 21:05:19 +0900

    偶数であるか判定する関数を追加

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ function isMultipleOfSeventeen(num) {
   return num % 17 === 0;
 }
 
+function isEven(num) {
+  return num % 2 === 0;
+}
+
 module.exports = {
-  isMultipleOfSeventeen
+  isMultipleOfSeventeen,
+  isEven
 };
--- test.js
+++ test.js
@@ -1,8 +1,9 @@
 'use strict';
-const seventeen = require('./index.js');
+const judgement = require('./index.js');
 const assert = require('assert');
 
 const a = [83, 32, 85, 47, 77, 8, 61, 74, 29, 34, 11, 76, 60, 99, 55, 7, 19, 60, 98, 38, 28, 96, 32];
-assert.deepEqual(a.filter(seventeen.isMultipleOfSeventeen), [85, 34]);
+assert.deepEqual(a.filter(judgement.isMultipleOfSeventeen), [85, 34]);
+assert.deepEqual(a.filter(judgement.isEven), [32, 8, 74, 34, 76, 60, 60, 98, 38, 28, 96, 32])
 
 console.log('テストが正常に完了しました');
