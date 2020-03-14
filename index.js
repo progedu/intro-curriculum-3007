f8736fc8f73ce7f700f72b61855af0b8f5f4c254
Megumi <megumi67@gmail.com>
2020-03-14 18:55:33 +0900

    17で割り切れるか？

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
+/**
+ * １７の倍数ならtrueを返す
+ * @param {number} num
+ */
+
+ function isMultipleOfSeaventeen(num){
+     return num % 17 === 0;
+ }
 
 module.exports = {
-}
+    isMultipleOfSeaventeen
+};
