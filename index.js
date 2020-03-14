fc8e9a233e6454eb285a7628d3f549bab275031d
soleil1986 <soleil.taiyo@gmail.com>
2020-03-14 23:25:53 +0000

    モジュールの実装(trueを返すのは、条件判定だけでOK)

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 'use strict';
 
+/**
+* number配列を受け取り、真偽値を返す
+* @param {number} number
+* @return {boolean} 17で割り切れるか
+*/
+// function isMultipleOfSeventeen(number){
+//   if(number % 17 === 0){
+//     return true;
+//   };
+// }
+function isMultipleOfSeventeen(number){
+  return number % 17 === 0;
+}
+
 module.exports = {
+  isMultipleOfSeventeen
 }
