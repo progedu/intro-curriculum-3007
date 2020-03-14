e558333338847a2515e6049ef1df1a8ff9272db3
tora272 <nao2000ya@yahoo.co.jp>
2020-03-15 07:59:20 +0900

    17の倍数判定の実装

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
+/**
+ * 17で割り切れるかどうかを判定する
+ * @param {number} num
+ * @return {boolean} 17で割り切れる物のみ
+ */
+function isMultipleOfSeventeen(num) {
+    return num % 17 ===0;
+}
 
 module.exports = {
-}
+    isMultipleOfSeventeen
+};
