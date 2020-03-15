7fbbe0a62c5823ef52b5c6a6474a7a437ebe3772
dnishio <dnish240@gmail.com>
2020-03-15 14:21:34 +0900

    17の倍数かどうか判定する関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 'use strict';
-
+/**
+ * 17で割り切れる場合 true を返す関数
+ * @param {number} num 
+ */
+function isMultipleOfSeventeen(num){
+    return num % 17 === 0;
+}
 module.exports = {
+    isMultipleOfSeventeen,
 }
