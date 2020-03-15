efb1f2aae2622871e6158870e41d934f84a1eba0
Fumiya007 <Fumiya007@users.noreply.github.com>
2020-03-15 20:27:05 +0900

    isMultipleOfSeventeen実装

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
+/**
+ * 17の倍数の場合はtrueを返す
+ * @return {number} n
+ */
+function isMultipleOfSeventeen(n){
+    return n % 17 === 0;
+}
+
 module.exports = {
+    isMultipleOfSeventeen
 }
