7a9eab048d0e9ec4cae1fe2901363d175d1dd563
ururunya <ururunya@gmail.com>
2020-03-17 22:13:55 +0900

    seventeenモジュールの実装

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 'use strict';
 
+/**
+ * 配列を17で割り切れる数のみの配列にする
+ * @param {number} integralValue
+ * @return {boolean} 17で割り切れるかどうか
+ */
+
+function isMultipleOfSeventeen(integralValue) {
+  return integralValue % 17 === 0
+}
+
 module.exports = {
+  isMultipleOfSeventeen
 }
