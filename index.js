e2cec18e3546777fdae0e9d71a0029a2cd758381
kohei <kouhik06@gmail.com>
2020-03-22 03:47:54 +0900

    整数が 17 で割り切れるものだけにする seventeen モジュールを実装

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
 
-module.exports = {
+/**
+ * 整数が 17 で割り切れるかどうかを判定する
+ * @param {number} num
+ * @return {boolean} 17で割り切れるかどうか
+ */
+function isMultipleOfSeventeen(num) {
+  return num % 17 === 0;
 }
+
+module.exports = { isMultipleOfSeventeen };
