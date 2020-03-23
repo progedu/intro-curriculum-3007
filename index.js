761d8ee0f11b72ed452e36d5f8115f93e99e3ac1
chc1129 <chc1129@gmail.com>
2020-03-24 06:58:56 +0900

    isMultipleOfSeventeen関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
+/**
+ * 整数が17で割り切れるか判定する
+ * @param {num}
+ * @return {boolean}
+ */
+function isMultipleOfSeventeen(num) {
+    return ((num % 17) === 0);
+}
+
 module.exports = {
+    isMultipleOfSeventeen
 }
