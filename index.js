082d94a1f165d53eb71eec3f8f97447768285e07
koume <koume138@gmail.com>
2020-03-24 22:45:13 +0900

    17で割り切れるか判定する関数を登録

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 'use strict';
-
-module.exports = {
+/**
+17 で割り切れる(true)か否か(false)を返す 
+* @param {array} a
+* @return {boolean}   
+*/
+function isMultipleOfSeventeen(a){
+　  return (a % 17 == 0);
 }
+module.exports = { isMultipleOfSeventeen };
