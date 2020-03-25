916460b288fa5c18494988393fa49b27e7f401d7
nek <utbhttmk@gmail.com>
2020-03-25 23:08:47 +0900

    isMultipleOfSeventeen関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
+/**
+ * @param {number} num
+ */
+function isMultipleOfSeventeen(num) {
+  if (num % 17 === 0) {
+    return num;    
+  }
+}
 
 module.exports = {
-}
+  isMultipleOfSeventeen
+};
