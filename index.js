bdf0684b36ad550a38e963bc5f34508394176e0a
nkou aoto <aoto_18n1100261@nnn.ed.jp>
2020-03-27 16:31:42 +0900

    isMultipleOfSeventeen 関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 'use strict';
 
+function isMultipleOfSeventeen(num){
+    return num %17 ===0;
+}
+/**
+ * 17の倍数の場合trueを返す
+ * @param {number} num
+ */
+
+
+
 module.exports = {
+    isMultipleOfSeventeen
 }
