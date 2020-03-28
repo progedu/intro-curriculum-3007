b2f39546f5f2673f5e34b7067a0c63c25534359e
nandemotoken <nandemotoken@gmail.com>
2020-03-28 14:12:04 +0900

    my answer

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 'use strict';
 
+function isMultipleOfSeventeen(val){
+    console.log("value" + val)
+
+//    console.log(a.list())
+    // return Array.from([])
+    // const b = Array.from([ 3 ,17 , 34]).filter( n => n % 17 === 0 )
+    // const a = Array.from(args).filter( n => n % 17 === 0 )
+//    console.log(a)
+    return val % 17 ===0
+    //    return[ 85 , 34]
+}
+
 module.exports = {
+    isMultipleOfSeventeen
 }
