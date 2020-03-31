29ccbd989e3df294da77f1d07ba59bed97e28431
hartmann-h <mr.hydrogen2@gmail.com>
2020-03-31 21:37:09 +0900

    関数isMultipleOfSeventeenを実装

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
+/**
+ * 整数を受け取り、17で割り切れるかどうかを返す
+ * @param {number}
+ * @return {boolean}
+ */
+function isMultipleOfSeventeen(number) {
+  return number % 17 === 0;
+}
+
 module.exports = {
+  isMultipleOfSeventeen
 }
