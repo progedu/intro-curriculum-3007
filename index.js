7d68044e8addf5a864ff234d0fde1b50faf95cfa
sousvillage <sousvillage@gmail.com>
2020-03-31 17:01:05 +0900

    isMultipleOfSeventeen関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
+/**
+ * 整数が17で割り切れるかどうかを判定する
+ * @param {Number} num
+ * @return {boolean} 17で割り切れるか
+ */
+function isMultipleOfSeventeen(num) {
+    return num % 17 === 0;
+}
 
 module.exports = {
-}
+    isMultipleOfSeventeen
+};
