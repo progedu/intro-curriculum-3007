28399f0a89aeafa3efc9ddab8f63cec927305ad3
tomohiro <tomohiroakazawa@gmail.com>
2020-04-04 19:33:48 +0900

    modified

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 'use strict';
 
-module.exports = {
+ /**
+  * @return {boolean}
+  */
+ function isMultipleOfSeventeen(num) {
+     return num % 17 == 0;
+ }
+
+module.exports = { isMultipleOfSeventeen
 }
