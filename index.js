a7fbf921972eebe998deba2f3555b24d49229f0e
kajiyama <kajiyama@gmail.com>
2020-04-05 04:30:47 +0000

    ぬるぽ

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
 
-module.exports = {
+/**
+ * 17で割り切れる数字を判定する
+ * @param {number} 
+ * @return {boolean} : split of 17
+ */
+function isMultipleOfSeventeen(number) {
+    return number % 17 === 0 ? true : false;
 }
+
+module.exports = {isMultipleOfSeventeen}
