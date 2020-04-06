021673934134a9f90769c95cbf4ef3087ed0f406
yugei806 <sennpuuki810@gmaile.com>
2020-04-06 14:39:43 +0000

    関数の追加

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 'use strict';
 
+/*
+*17の倍数である場合 true を返す
+* @param {number} number
+*/
+
+function isMultipleOfSeventeen(number){
+    if(number % 17 === 0){
+        return true;
+    }
+    return false;
+}
+
+
 module.exports = {
+    isMultipleOfSeventeen
 }
