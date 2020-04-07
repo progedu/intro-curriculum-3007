bcf269fe9678b9435c05fc9920bef7bc673a3ad9
HidakaRintaro <rintaro0411bus@gmail.com>
2020-04-07 15:39:19 +0900

    17の倍数を求める関数の実装完了

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 'use strict';
-
-module.exports = {
+/**
+ * 整数が17で割り切るかどうかを返す
+ * @param {number} num
+ */
+function isMultipleOfSeventeen(num) {
+  return num % 17 === 0;
 }
+
+module.exports = { isMultipleOfSeventeen };
