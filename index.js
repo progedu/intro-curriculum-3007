a58d1162d9ba5ab095fe00824ead92ff6968f2af
Yusuke Ichihara <ichihara.yusuke.17@shizuoka.ac.jp>
2020-04-14 22:54:03 +0900

    17の倍数判定関数を作成

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
+/**
+* 17で割り切れる場合 true を返す
+* @param {num} numArray
+*/
+function isMultipleOfSeventeen(num){
+    return n % 17 === 0;
+}
 
 module.exports = {
+    isMultipleOfSeventeen
 }
