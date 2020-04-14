5b7150a9b681b766e4687c251828793226148481
May-Rode <ryokun.monhan@gmail.com>
2020-04-14 13:46:10 +0900

    17割切れるか判定実装

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
+/**
+ * isMultipleOfSeventeen関数の実装
+ * @param {array} unmber
+ * @return {boolean}
+ */
+ function isMultipleOfSeventeen(number){
+    return number%17===0;
+ }
 module.exports = {
-}
+    isMultipleOfSeventeen
+};
