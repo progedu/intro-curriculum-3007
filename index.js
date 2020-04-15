283553bf2ebd6ecf3b5cdbbce6e0e28d58299958
Michiru Asano <michiru.0715@live.jp>
2020-04-15 11:45:34 +0900

    isMultipleOfSeventeen 関数のモジュールを実装

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 'use strict';
-
-module.exports = {
+/*＊
+* 17の倍数の場合、trueを返す
+* @param {number} num
+*/
+function isMultipleOfSeventeen(num){
+ return num % 17 === 0;
 }
+
+module.exports = { isMultipleOfSeventeen };
