970a28c089236838c65c78ef63b6bea582c3bdf1
YuuYabu <wongty0220@gmail.com>
2020-04-16 19:48:45 +0900

    isMultipleOfSeventeen関数を実装

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
+const numArray = new Array;
+
+/**
+ * 17の倍数である場合trueを返す
+ * @param {number} num
+ */
+function isMultipleOfSeventeen(num) {
+    return num % 17 === 0;
+}
 
 module.exports = {
+    isMultipleOfSeventeen
 }
