4f7d43351c3dcfe920274dc71d0728fcc8717fe8
cosimafromhell <fjhshrm@gmail.com>
2020-04-16 18:32:48 +0900

    17の倍数かを判定する関数を実装

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
+/**
+ * 17の倍数であれば true を返す
+ * @param {number} num
+ */
+function isMultipleOfSeventeen(num) {
+    const judge = (num % 17 === 0);
+    return judge;
+}
 
 module.exports = {
+    isMultipleOfSeventeen
 }
