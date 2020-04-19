f417a1255349049332c00b81c0bb6813f17182af
zavi3 <duna.blue@gmail.com>
2020-04-19 17:18:56 +0900

    17割切判定モジュールの実装

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 'use strict';
+// 配列に含まれる整数が 17 で割り切れるものだけにする seventeen モジュールを実装
+// 整数が 17 で割り切れるかどうかを判定する isMultipleOfSeventeen 関数
 
-module.exports = {
+const num = new Map();
+
+function seventeen(num) {
+    numbers.set(num, false);
+}
+
+function isMultipleOfSeventeen(num) {
+    if (num % 17 === 0) {
+        return  true;
+    } else {
+        return false;
+    }
+}
+
+
+
+module.exports = { 
+    isMultipleOfSeventeen
 }
