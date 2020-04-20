82ebda4be4bcde620f278e8fd31c60c3449006c1
Mari Miyata <mi.ma.11280@gmail.com>
2020-04-20 06:20:08 +0000

    Add seventeen module

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 'use strict';
 
-module.exports = {
+/**
+ * Check if the number is multiple of 17.
+ * @param {number} num
+ */
+function isMultipleOfSeventeen(num) {
+    return num % 17 === 0;
 }
+
+module.exports = { isMultipleOfSeventeen };
