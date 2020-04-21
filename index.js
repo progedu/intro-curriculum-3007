bb3f3f868bd7379590da5c34480222d46672787f
applego <applegorillappa@gmail.com>
2020-04-21 16:42:56 +0900

    17で割り切れるかどうかを返す関数

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
+/**
+ * 配列に含まれる整数が17で割り切れるものだけにする
+ * @param {Number}
+ * @return {boolean} 17で割り切れるかどうか
+ */
+function isMultipleOfSeventeen(n) {
+    return n % 17 === 0;
+}
 
 module.exports = {
-}
+  isMultipleOfSeventeen
+};
