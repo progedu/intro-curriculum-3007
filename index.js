e1b4960d6d1a80c87024ba13ee339e0282b1b092
Masaki <sbtmkym@yahoo.co.jp>
2020-04-25 17:02:40 +0900

    add seventeen

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
+/**
+ * divided by 17, retrun true
+ * @param {number} num
+ */
+
+function isMultipleOfSeventeen(num) {
+    return num % 17 == 0;
+}
 
 module.exports = {
-}
+    isMultipleOfSeventeen
+};
