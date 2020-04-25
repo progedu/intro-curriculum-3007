953e7fe3038e40c68d039a9e2f88efc879cd37dd
ilzamact <lozoot917@gmail.com>
2020-04-25 13:59:42 +0000

    17の倍数だけ返す機能実装

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
+// 整数が17で割り切れるかどうか判定する関数
 'use strict';
 
+//key: 数字 value
+const nums = new Map();
+
+/**
+ * 配列のうち17で割り切れないものをfalseに変える
+ * @param {array} num 
+ * @return {array} 
+ */
+function isMultipleOfSeventeen(num){
+    var bool = null;
+//    console.log(num.length); //配列で渡されていない(?)からundefined
+//    for(var i = 0; i < num.length; i++){ //array.filterが各要素を個別に渡してくれるのでforは不要??
+    //    let e[i] = false;
+        if(num % 17 != 0){
+            bool = false;
+        }else{
+            bool = true;
+        }
+//    }
+    console.log(num);
+    console.log(bool);
+    return bool;
+}
+
 module.exports = {
+    isMultipleOfSeventeen
+    
 }
