7a3e0191cbfae37db88e21340cd61ae4f919c25f
Haggy Kusanagi <artkit44t@gmail.com>
2020-04-30 14:32:35 +0900

    seventeen モジュールを実装

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
-module.exports = {
+/**
+ * 17で割り切れるかどうかが判定する
+ * @param {number} num
+ * @return {boolean} 17で割り切れるかどうか
+ */
+function isMultipleOfSeventeen(num){
+    return num % 17 === 0 ;
 }
+
+module.exports = {
+    isMultipleOfSeventeen
+};
