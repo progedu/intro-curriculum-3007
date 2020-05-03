46fff84e8c8fb2c9a19efa650eb069ceea8d8b6b
Shumpei Fukui <shunpei.f.0408@gmail.com>
2020-05-03 18:20:52 +0900

    isMultipleOfSeventeen関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 'use strict';
-
-module.exports = {
+/**
+ * 引数が17の倍数かどうかを判定
+ * @param {int} n 
+ */
+function isMultipleOfSeventeen(n) {
+    return n % 17 === 0;
 }
+
+module.exports = { isMultipleOfSeventeen };
