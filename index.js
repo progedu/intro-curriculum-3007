fc57e5776936afe17c3cfbf10f5c7916a5155f77
nakako <nakako.soft@gmail.com>
2020-05-04 16:09:15 +0900

    17で割り切れるかを判定する関数の追加

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
+/**
+ * 17で割り切れるかを調べる関数
+ * @param {number} num  (調べる値)
+ * @return 割り切れるかどうか（true: 割り切れる, false: 割り切れない)
+ */
+function isMultipleOfSeventeen(num){
+    return num%17 === 0;
+}
 
 module.exports = {
+    isMultipleOfSeventeen
 }
