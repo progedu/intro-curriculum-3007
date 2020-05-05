aba96a29de1e55aedbdd561bd832fc4d92160882
sayoko <discegaudere.blackberry@gmail.com>
2020-05-05 09:37:39 +0000

    isMultipleOfSeventeenのじっそう

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
 
-module.exports = {
-}
+function isMultipleOfSeventeen(number) {
+
+    // if (number%17 === 0) {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+    return number % 17 === 0;
+  }
+module.exports = {isMultipleOfSeventeen}
