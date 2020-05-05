beba76623fbf7da26ef62c10f605c3043b4502f7
liwintg <chen_m_maze@yahoo.co.jp>
2020-05-05 15:47:07 +0900

    整数が17で割り切れるか判定する関数実装

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
 
-module.exports = {
+/**17の倍数であるときのみtrue
+* @param {number} num
+*/
+function isMultipleOfSeventeen(num) {
+	return num % 17 === 0;
 }
+
+module.exports = {
+	isMultipleOfSeventeen
+};
