632be2008620b3a490f852c436ce6ca823bd2be5
n-hgc <gpxxakag@gmail.com>
2020-05-05 14:44:21 +0900

    isMultipleOfSeventeen関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
+/** 17で割り切れる数かどうかを返す
+* @param {int}
+* @return {boolean}
+*/
+function isMultipleOfSeventeen(num) {
+	return num % 17 === 0;
+}
+
 module.exports = {
+	isMultipleOfSeventeen
 }
