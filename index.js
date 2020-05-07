d5ff5b39c8d72b501269a47a55139140aa9209f2
second <second_boy@icloud.com>
2020-05-07 20:37:20 +0900

    isMultipleOfSeventeen関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 'use strict';
+/**
+ * @param {number} integer 
+ */
+function isMultipleOfSeventeen(integer) {
+    return integer % 17 === 0;
+}
 
 module.exports = {
+    isMultipleOfSeventeen
 }
