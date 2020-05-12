dadd2388a473ef30648b2cc9dbc59c2167ab1c07
NERK00426 <nanase.matsui@gmail.com>
2020-05-12 12:44:32 +0200

    isMultipleOfSeventeen関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+
 'use strict';
 
-module.exports = {
+/**
+ * 17の倍数である場合trueを返す
+ * @param {number}num
+ */
+
+function isMultipleOfSeventeen (num) {
+    return num % 17 === 0;
 }
+
+
+module.exports = { 
+    isMultipleOfSeventeen
+};
