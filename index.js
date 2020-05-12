8bc66c8e63d95928c4e02a53cd94753f421b58e4
Kosuke Maruyama <maruyama.04100410@gmail.com>
2020-05-12 23:11:56 +0900

    isMultipleOfSeventeen 関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
-module.exports = {
+/**
+ * 整数が17で割り切れるかどうかを判定する
+ * @param {number} number
+ * @return {boolen} 17で割り切れるかどうか
+ */
+function isMultipleOfSeventeen(number) {
+    return number % 17 === 0;
+}
+
+module.exports = { isMultipleOfSeventeen
 }
