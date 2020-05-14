0d0fc8e646fbb5fa30838a16613fc11a3ed7c3ce
ace-yamano <akira.yamano@veriserve.co.jp>
2020-05-14 17:12:24 +0900

    17で割り切れるかどうか関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
+/**
+ * 17 の倍数の時に真を返す関数
+ * @param {number}
+ */
+function isMultipleOfSeventeen(num) {
+    return num%17 === 0;
+}
 
 module.exports = {
-}
+    isMultipleOfSeventeen
+};
