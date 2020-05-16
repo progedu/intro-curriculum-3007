e0a91480186c782e6d90000030e4eeaac4427d09
melissa <e-panjaitan@xware.co.jp>
2020-05-16 22:42:50 +0900

    function seventeen

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 'use strict';
-
-module.exports = {
+/**
+ * enable number to be divided by 17
+ */
+function isMultipleOfSeventeen(num){
+    return num%17 === 0;
 }
+
+module.exports = { isMultipleOfSeventeen};
