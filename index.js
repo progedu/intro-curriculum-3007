3545c62b1a13311dafadfd7eadc2f21189cd2a60
hashi-oki <sushikuite77@gmail.com>
2020-05-19 23:23:52 +0900

    １７で割り切れるかどうか判定するモジュールを実装

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
+/**
+ * １７で割り切れるかどうか判定する。
+ * @return {number} 
+ */
+function isMultipleOfSeventeen(number) {
+    return number % 17 === 0;
+}
 
 module.exports = {
+    isMultipleOfSeventeen
 }
