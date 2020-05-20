3e092a21d4bcd539cd6b50b12d5ff8314cce174f
Tomoya Kanemaru <pandora.border@gmail.com>
2020-05-20 21:54:25 +0900

    整数が17で割り切れるかを確認する関数を実装

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
 
-module.exports = {
+/**
+ * 整数が17で割り切れるかを取得
+ * @param {int} number
+ * @return {bool} true:割り切れる / false:割り切れない
+ */
+function isMultipleOfSeventeen(number){
+    return 0 === number % 17;
 }
+
+module.exports = { isMultipleOfSeventeen };
