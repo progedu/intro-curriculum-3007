1455bb376976b10859cd27e3ac7adecf04b10ad9
nafuka <42476527+nafuka11@users.noreply.github.com>
2020-05-20 18:37:02 +0900

    Feat: isMultipleOfSeventeen()追加

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
+/**
+ * 整数が17で割り切れるかどうか判定する
+ * @param {number} num 整数
+ * @returns {boolean} true: 割り切れる, false: 割り切れない
+ */
+function isMultipleOfSeventeen(num) {
+  return num % 17 === 0;
+}
+
 module.exports = {
+  isMultipleOfSeventeen
 }
