c6225beabb6fcdaac127075faad1ebc350c0802b
tagamiwork <uedawork2020@gmail.com>
2020-05-22 10:36:11 +0900

    17で割り切れる数の確認処理を実装

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
+/**17の倍数である場合 true を返す
+ * 
+ * @param {number} num 
+*/
+function isMultipleOfSeventeen (num) {
+  return num % 17 === 0;
+}// isMultipleOfSeventeen()
+
+// パッケージの関数として外部に公開する
 module.exports = {
-}
+  isMultipleOfSeventeen
+};
