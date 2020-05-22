83834d44d7e25523bd5c9d79298306a384901e05
taigaozawa <taiga_ozawa@dwango.co.jp>
2020-05-23 08:09:24 +0900

    isMultipleOfSeventeen 関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
 
-module.exports = {
+/**
+ * 数値を受け取ってそれが 17 の倍数であるかを判定する関数
+ * @param {num} num
+ * @return {boolean}
+ */
+function isMultipleOfSeventeen(num) {
+   return num % 17 === 0;
 }
+
+module.exports = { isMultipleOfSeventeen };
