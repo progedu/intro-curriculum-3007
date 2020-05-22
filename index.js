7d1f158af7c73a824efb1fdf0f1122400b8ae4ea
kazu <vanillaver111@gmail.com>
2020-05-22 17:24:44 +0900

    17で割り切れるかどうかの判定

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 'use strict';
-
+/**
+ * 数字の配列を受け取り、17で割り切れる場合、trueを返す
+ * @param {number} num
+ */
+function isMultipleOfSeventeen(num) {
+    return num % 17 === 0;
+}
 module.exports = {
+    isMultipleOfSeventeen
 }
