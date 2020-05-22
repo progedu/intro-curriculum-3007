0c0a36ca71a44b30de3962e32618d48c3cb90504
mopcup <mopcup.jp@gmail.com>
2020-05-22 17:11:08 +0900

    isMultipleOfSeventeen関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
-module.exports = {
+/**
+ * numberが17で割り切れるかどうかを判定する
+ * @param {Number} number 
+ * @return {boolean} numberが17で割り切れるかどうか
+ */
+function isMultipleOfSeventeen(number) {
+  return number % 17 === 0;
+}
+
+module.exports = { isMultipleOfSeventeen
 }
