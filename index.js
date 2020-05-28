a4a3b9ab73c81852e29206f2266c3622b3cd0c4d
Sae Amano <shinn28jetblack@gmail.com>
2020-05-28 14:19:12 +0900

    isMultipleOfSeventeen 関数

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
+/**
+* TODOの一覧の配列を取得する
+* @param {number} num
+* @return {boolean}
+*/
+function isMultipleOfSeventeen(num) {
+    return num % 17 == 0;
+  }
 
 module.exports = {
+    isMultipleOfSeventeen
 }
