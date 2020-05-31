a193d210362c83d8cf29feee690b5e1ee1f57a32
Kenta K <i.never.read.your.emails@gmail.com>
2020-05-31 16:28:52 +0900

    練習問題

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 'use strict';
+/**
+ * 17の倍数である時にtrueを返す
+ * @param {number} nums 
+ */
 
-module.exports = {
-}
+function isMultipleOfSeventeen(nums) {
+    return nums % 17 === 0;
+};
+
+module.exports = { isMultipleOfSeventeen }
