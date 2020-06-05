a4e169233a79a0a7629f44a171ad731eefa61b0c
abe <raifo1991@yahoo.co.jp>
2020-06-05 19:55:10 +0900

    練習問題を解いた

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
+/**
+ * 与えられた数字が17で割り切れる時trueを返す
+ * @param {int} n
+ */
+function isMultipleOfSeventeen(n) {
+    return n % 17 == 0
+}
 
 module.exports = {
+    isMultipleOfSeventeen
 }
