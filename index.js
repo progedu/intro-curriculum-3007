344a9640b6869df1176ab99b08e284180bc629ec
yrberrytea <yrberrytea@gmail.com>
2020-06-07 13:46:29 +0900

    Create isMultipleOfSeventeen function

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
+/**
+* 整数を受け取り、17で割り切れるか否かを判定
+* @param {integer} n
+* @return {boolean}
+*/
+function isMultipleOfSeventeen(n) {
+    return n % 17 === 0;
+  }
+
 module.exports = {
+    isMultipleOfSeventeen
 }
