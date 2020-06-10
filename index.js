5e38683df3fc9b6323370c13b5e4891cfddfd6a4
Harumi <hru.o6o4@gmail.com>
2020-06-10 14:45:20 +0900

    17で割り切れるかどうかを返す関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-'use strict';
+"use strict";
 
-module.exports = {
+const seventeen = new Map();
+
+/**
+ * @param { number } num
+ * @return 17で割り切れるかどうかを返す真偽値
+ */
+
+function isMultipleOfSeventeen(num) {
+  return num % 17 === 0;
 }
+
+module.exports = { isMultipleOfSeventeen };
