f78dc5481072eb2ce84deea6a9080cf7736b0af0
吉田稲作 <yoshidanoinasaku@gmail.com>
2020-06-14 15:48:41 +0900

    17で割り切れるか判定するモジュールの実装

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 'use strict';
 
+
+/**
+ * 17で割り切れるかどうかを判定する
+ * @param {integer} number 
+ */
+function isMultipleOfSeventeen(number) {
+    let numbers = [];
+    if(number % 17 === 0) {
+        numbers.push(number);
+    }
+    return numbers;
+}
+
 module.exports = {
+    seventeen: isMultipleOfSeventeen
 }
