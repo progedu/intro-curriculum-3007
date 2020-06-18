4000c5b478c0f6e5793469f262cb7998e66980fb
nikoniko5963 <nikoniko2255n@gmail.com>
2020-06-18 19:03:39 +0900

    関数追加

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
+/**
+ * 渡された数値が17で割り切れるか確認 
+ * @param {number}
+ * @returns {boolean}
+ */
+function isMultipleOfSeventeen(num){
+    return num % 17 === 0;
+}
+
 module.exports = {
+    isMultipleOfSeventeen
 }
