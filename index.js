b2078a8be07f4c43e52cf587948301302123ec36
torajiko <torajiko@protonmail.ch>
2020-06-26 00:40:39 +0900

    17で割り切れるかどうかを判定する関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
+/**
+ * 17で割り切れる場合、TRUEの値を返す
+ * @param {number} number
+ */
+function isMultipleOfSeventeen(number){
+    return number % 17 === 0;   
+}
+
 module.exports = {
+    isMultipleOfSeventeen
 }
