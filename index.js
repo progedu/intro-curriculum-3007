c75a99886b03be9c908d965c4e49194a68de9757
nooneknows2020 <e_dends@yahoo.co.jp>
2020-06-25 22:36:04 +0900

    配列に含まれる整数が 17 で割り切れるものだけにする seventeen モジュールを実装

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
-module.exports = {
+/**
+ * 17の倍数である場合trueを返す
+ * @param {number} num
+ * @return {boolean}
+ */
+function isMultipleOfSeventeen(num){
+    return num % 17 === 0;
 }
+
+module.exports = {
+  isMultipleOfSeventeen
+};
