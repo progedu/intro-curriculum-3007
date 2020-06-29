d9d4008022a86283e2e6961970e756a3169bb097
Makino Yudi <hymakino@icloud.com>
2020-06-30 02:20:18 +0900

    途中経過

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
+const numbers = new Array();
+
+function addNumbers(a) {
+  numbers.push({ value: a});
+}
+
+function isMultipleOfSeventeen(numbers) {
+  
+}
 
 module.exports = {
 }
