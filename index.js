f606b037071bc9d46d1dc9dbc9c339f92f05bac8
chii2020 <chii1a@yahoo.co.jp>
2020-07-06 15:45:58 +0900

    commit

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
 'use strict';
-
-module.exports = {
+/**
+* 17の倍数である場合 true を返す
+* @param {number} num
+*/
+function isMultipleOfSeventeen(num) {
+  return num % 17 === 0;
 }
+console.log(isMultipleOfSeventeen(34));
+
+module.exports = { 
+  isMultipleOfSeventeen
+};
+
+/*
+function isMultipleOfSeventeen2() {
+    const number = process.argv[2] || 0;
+    let sum = 0;
+    for (let i = 1; i <= number; i++) {
+        sum = sum + i;
+        if (sum % 17 === 0) {
+            console.log(sum);
+        }
+    }
+}
+isMultipleOfSeventeen2();
+*/
