8ec20e7e68a018666efc0c8a249f0245d240785d
omochi <pink.camel.ingress@gmail.com>
2020-07-26 23:26:13 +0000

    17の倍数かを判定するメソッドを実装

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
+/**
+ * 17の倍数かの判定
+ * @param {number} num
+ * @returns {boolean} 17の倍数のときtrueを返す
+ */
+function isMultipleOfSeventeen(num) {
+    return (num % 17) == 0;
+}
+
 module.exports = {
+    isMultipleOfSeventeen
 }
