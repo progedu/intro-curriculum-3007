984e2e5cabf3f49e951a25cc0eb26087ffc5ef6a
jess285 <ozawa9684@gmail.com>
2020-08-09 10:50:41 +0900

    seventeenモジュールの実装

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
+/**
+ * 17の場合trueを返す
+ * @param {number}num
+ */
+ 
+ function isMultipleOfSeventeen(num){
+   return num % 17 === 0;
+ }
+
 module.exports = {
-}
+  isMultipleOfSeventeen
+};
