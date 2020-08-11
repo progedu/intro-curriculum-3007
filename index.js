d08cb98f2cad5e138afbf15bfdecda6872bc750b
vagrant <vagrant@vagrant.vm>
2020-08-11 15:22:35 +0900

    17で割り切るるかの判定モジュール実装

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 'use strict';
 
+/***
+ * 17で割り切れる場合Trueを返す。
+ * @param {Array} 
+ * @return 
+ */
+function isMultipleOfSeventeen(num) {
+    return (num % 17 === 0);
+}
+
+
 module.exports = {
+    isMultipleOfSeventeen
 }
