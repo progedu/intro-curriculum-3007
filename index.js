450e7b94d8546e6d0718e7667ea358b6fcea365a
Kotaro Hasegawa <hachocho2198@gmail.com>
2020-08-11 16:21:21 +0900

    isMultipleOfSeventeenを実装

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 'use strict';
 
-module.exports = {
+/**
+ * 整数が17で割り切れるなら true を返す
+ * @param {number} num
+ */
+function isMultipleOfSeventeen(num) {
+    // if (num % 17 === 0){
+    //     return true;
+    // }else{
+    //     return false;
+    // }
+    return num % 17 === 0;
 }
+
+module.exports = {
+    isMultipleOfSeventeen
+};
