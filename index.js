8aeee4b095f8090ca135895c31ab44a74ee193fb
Kiyoto Akazawa <kiyoto.a328@docomo.ne.jp>
2020-08-12 17:06:47 +0900

    17の倍数で割り切れる値を抽出機能の追加

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 'use strict';
+//{num:数値、state: 割り切れるかどうかの真偽値}
+const numbers=new Array();
+
+/**
+ * 17の倍数である場合trueを返す
+ * @param {numbet} num 
+ */
+function isMultipleOfSeventeen(num){
+    return num%17===0;
+}
 
 module.exports = {
+    isMultipleOfSeventeen
 }
