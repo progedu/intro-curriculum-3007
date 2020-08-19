d26486a404dae04baaa35883bae7ee8c64093fd4
Shion Nakajima <sio.nakaj@gmail.com>
2020-08-19 15:55:21 +0900

    提出

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
+/**
+ * 整数が17で割り切れるかどうかを判定
+ * @param {number} num
+ * @return {boolean}
+ */
+function isMultipleOfSeventeen(num){
+    return num%17 === 0;
+}
+
 module.exports = {
+    isMultipleOfSeventeen
 }
