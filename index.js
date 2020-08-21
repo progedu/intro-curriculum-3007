70f80b61bce986368e33ec45fc74a6422de81388
Ryoken Nagase <mtsza.mtznh@gmail.com>
2020-08-21 18:35:04 +0900

    17で割り切れるかの判定するisMultipleOfSeventeenの実装

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 'use strict';
 
+/**
+ * 整数が17で割り切れるかを判定する
+ * @param {int} input
+ * @return {boolean} output
+ */
+function isMultipleOfSeventeen (input) {
+    if (input % 17 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 module.exports = {
+    isMultipleOfSeventeen
 }
