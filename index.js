6372df956554837d0d2b8d84437818b1a9e6f926
mtmtP <matsumoto0916@gmail.com>
2020-08-26 23:28:40 +0900

    17の倍数判定関数を実装

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
+/**
+ * 17で割れる数だけを配列にするやつ
+ * @param {number} num
+ */
+function isMultipleOfSeventeen(num){
+    return num % 17 === 0;
+}
 
 module.exports = {
-}
+    isMultipleOfSeventeen
+};
