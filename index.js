e606212d849e4d2652c38423e064c319eac56668
MatsunamiKaito <kaito.matsunami0524@gmail.com>
2020-09-08 21:59:00 +0900

    0908

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 'use strict';
+/**
+ * @param {array} input
+ * @return {array} 
+ * 
+ * 渡された配列を探索して、17で割れるものだけを拾って新しい配列に入れて返す。
+ *
+ */
+const isMultipleOfSeventeen=(input)=>{
+    return (input%17 === 0);
+};
+
 
 module.exports = {
+    isMultipleOfSeventeen
 }
+
