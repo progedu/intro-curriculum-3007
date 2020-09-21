91015e4e1ac546aabf0e197256a5ac769cf67e8d
ck-n2020 <ckckck_009@yahoo.co.jp>
2020-09-21 23:26:19 +0900

    数字が17の倍数であるかを判定する関数を実装

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
+/**
+ * 数字を受け取り、17で割り切れるかどうかを返す
+ * @param {Number} number
+ */
+function isMultipleOfSeventeen(number) {
+    return number % 17 === 0;
+}
+
 module.exports = {
+    isMultipleOfSeventeen
 }
