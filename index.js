6ee2d4c2e814cc9d2ad9dbad86dec87a5122b3ef
高橋宏輝 <chanko_85@yahoo.co.jp>
2020-09-26 11:20:59 +0900

    整数が17で割り切れるかを判定する関数を追加

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
+/**
+ * 整数が17で割り切れるか判定する
+ * @param {int} n
+ * @return {boolean} 割り切れるか？
+ */
+function isMultipleOfSeventeen(n) {
+    return n % 17 === 0;
+}
+
 module.exports = {
+    isMultipleOfSeventeen
 }
