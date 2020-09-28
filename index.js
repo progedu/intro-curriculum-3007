aaa6174595b8c140257563385299c30ba58759fe
HappyKumamoto <zen.word+hub@gmail.com>
2020-09-28 15:05:26 +0900

    seventeenモジュールの実装

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
+/**
+ * 17の倍数のみtrue
+ * multiple of num
+ * (num の倍数)
+ * @param {number} num
+ */
+function isMultipleOfSeventeen(num) {
+  return num % 17 === 0;
+}
 
 module.exports = {
-}
+  isMultipleOfSeventeen
+};
