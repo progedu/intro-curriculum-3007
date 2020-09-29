deeaeb659776774d32af2afa5c1cffdc51abe5d4
theata <yudengdaotian@gmail.com>
2020-09-29 20:17:49 +0900

    17で割り切れる整数を識別する関数を17モジュールに実装

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 'use strict';
 
+/**
+ * 整数が17で割り切れるとき、trueを返す
+ * @param {number} int
+ */
+
+ function isMultipleOfSeventeen(int) {
+     return int % 17 === 0;　　　//17で割り切れるか検証
+ } 
+
+
 module.exports = {
-}
+    isMultipleOfSeventeen
+};
