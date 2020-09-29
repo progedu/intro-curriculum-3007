4182b528a679e265d807ef69f39996c735c549b7
Mieko Kobayashi <sutasutap@gmail.com>
2020-09-29 11:25:47 +0000

    練習問題

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
+/**
+ * 17の倍数をtrue で返す関数
+ * @param {number} 数値
+ * @returns{bool}
+ */
+function isMultipleOfSeventeen(num) {
+  return num % 17 === 0;
+}
 
 module.exports = {
-}
+  isMultipleOfSeventeen
+};
