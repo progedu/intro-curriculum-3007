80685dfff69bc9b892e33cf1c3450d15a750b360
natumenonomi <nonegemu3e@gmail.com>
2020-09-29 20:29:14 +0900

    seventeen モジュールを実装

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
-
+/**
+ * 17の倍数である場合　trueを返す
+ * @param {number}数値
+ * @returns {bool}
+ */
+function isMultipleOfSeventeen(number){
+  return number % 17 === 0;
+}
 module.exports = {
+ isMultipleOfSeventeen
 }
