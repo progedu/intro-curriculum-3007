4c868718575329b5bbcce8809b11c27cfd8e7bde
tatsuki yokoyama <unclesamforce@gmail.com>
2020-09-29 20:27:30 +0900

    １７の倍数追加

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 'use strict';
+/**
+ * 17の倍数である場合trueを返す
+ * @param {number} num 
+ * @returns{bool}
+ * 
+ */
+
+function isMultipleOfSeventeen(num){
+  return num % 17 === 0;
+}
+
+
 
 module.exports = {
+  isMultipleOfSeventeen
 }
