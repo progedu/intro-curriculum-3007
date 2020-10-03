99deef2d5fec3ff780ebdfc1b42bd3d0b483d6f4
shrine <shrine.game1@gmail.com>
2020-10-03 17:31:53 +0900

    3章08節の練習

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
 
-module.exports = {
+/**
+ * 17で割り切れる数値を true で返す
+ * @param {Number} num 
+ * @return {Boolean}
+ */
+function isMultipleOfSeventeen(num) {
+    return num % 17 === 0;
 }
+
+module.exports = { isMultipleOfSeventeen }
