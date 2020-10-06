c13695234770893d63085a31a166b4e1ea5a680c
samurai <xcybza155@gmail.com>
2020-10-06 18:49:13 +0900

    17の倍数をtrueで返す関数

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
+/**
+ * 17の倍数をtrue をで返す関数
+ * @param {number} 数値
+ * @return {bool}
+ */
+function isMultipleOfSeventeen(number) {
+    return number % 17 === 0;
+}
 
 module.exports = {
-}
+    isMultipleOfSeventeen
+};
