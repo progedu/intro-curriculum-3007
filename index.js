34dcdcf2af55a56a864926c7aede1145feeea8d7
terada takahiko <t.terada@sungroup.co.jp>
2020-10-13 13:56:19 +0900

    seventeenモジュールの実装

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
-module.exports = {
+/**
+ * 整数が 17 で割り切れるかどうかを判定する
+ * @param {number} number
+ * @return {boolean}
+ */
+function isMultipleOfSeventeen(number) {
+    return (number % 17 === 0);
 }
+
+module.exports = {
+    isMultipleOfSeventeen
+};
