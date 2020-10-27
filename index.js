1b721376158043ffeebde9ac3eaff3845ecd22a0
じん <gotwo525@gmail.com>
2020-10-27 18:11:05 +0900

    １７の倍数

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 'use strict';
-
-module.exports = {
+/**
+ * 17の倍数である場合　ture を返す
+ * @param{number}
+ */
+function isMultipleOfSeventeen(num){
+    return num % 17 === 0;
 }
+
+module.exports = { isMultipleOfSeventeen
+};
