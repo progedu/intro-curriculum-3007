c107ec3cfb6b78ecbc52f253419e4a519edf8cb2
Yuya Satake <sata.yu.7878@gmail.com>
2020-10-29 14:42:29 +0900

    made seventeen module

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
-
+/**
+ * 配列に含まれる整数が17で割り切れるものだけにする
+ * @params {num} num
+ * @return {array} * 
+ */
+function isMultipleOfSeventeen(num) {
+    return num % 17 === 0;
+}
 module.exports = {
+    isMultipleOfSeventeen
 }
