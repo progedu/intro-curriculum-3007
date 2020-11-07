b809d27719c6fbe8a93595c8585f308f3e3d34b7
ta-nu-ki3 <unplugged61662@gmail.com>
2020-11-08 07:23:07 +0900

    関数isMultipleOfSeventeenの実装

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 'use strict';
 
-module.exports = {
+/**
+ * 配列に含まれる整数が 17 で割り切れるものだけにする 
+ * @param {array} ary
+ * @return {array}
+ */
+function isMultipleOfSeventeen(ary) {
+  return ary % 17 === 0
 }
+module.exports = {
+  isMultipleOfSeventeen
+}
+
+// let a = [83, 32, 85, 47, 77, 8, 61, 74, 29, 34, 11, 76, 60, 99, 55, 7, 19, 60, 98, 38, 28, 96, 32]
+// const res = isMultipleOfSeventeen(a);
+// console.log(res);
