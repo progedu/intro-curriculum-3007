988e6b714bd5cacf450d82467b6354911a8905e9
Yu Kumagai <coffeekumasan@gmail.com>
2020-11-10 17:20:48 +0900

    isMultipleOfSeventeenの実装

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
+/**
+ * 17の倍数を返す関数
+ * @param {number} 数値
+ */
+function isMultipleOfSeventeen(nunber) {
+  return nunber % 17 === 0
+}
+
 module.exports = {
+  isMultipleOfSeventeen
 }
