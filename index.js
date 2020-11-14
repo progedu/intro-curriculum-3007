9e928786a1b9d7f94009949efb7f44df4544dc47
Naohiro Tomimura <white.noise.0000@gmail.com>
2020-11-14 23:14:04 +0900

    練習問題回答

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
+/**
+ * 配列要素が17で割り切れるか判定する.
+ * @param {int} item 
+ */
+function isMultipleOfSeventeen(item) {
+    return item % 17 === 0;
+}
+
 module.exports = {
+    isMultipleOfSeventeen
 }
