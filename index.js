2255de0972d1253ee407965654ed9b43e6f743b4
jonathanjoster <jonathanjoster0331@gmail.com>
2020-11-19 17:53:52 +0900

    completed

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
+/**
+ * 整数が17の倍数か判定する
+ * @param {int} n
+ * @return {boolean}
+ */
+function isMultipleOfSeventeen(n) {
+    return n % 17 === 0;
+}
 
 module.exports = {
+    isMultipleOfSeventeen
 }
