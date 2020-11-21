da98f1506d09a601f20e10237903ceacd0af2981
tama naoto <otama.engine@gmail.com>
2020-11-21 21:16:00 +0900

    isMultipleOfSeventeen関数を実装

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 'use strict';
 
+/**
+ * 整数が17で割り切れるかを返す
+ * @param {array} num
+ * @return {boolean} 17で割り切れるかどうか
+ */
+function isMultipleOfSeventeen(num) {
+  if (num % 17 === 0) {
+    return true;
+  }
+  return false;
+}
+
 module.exports = {
+  isMultipleOfSeventeen,
 }
