648d7d318d03549df6f39465682de29e08be5a43
Shori Take <wvwv0925@gmail.com>
2020-11-29 23:30:33 +0900

    seventeenモジュールの実装

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 'use strict';
 
-module.exports = {
+
+/**
+ * 17の倍数をtrueで返す関数
+ * @param {number} 数値
+ * @return {bool}
+ */
+function isMultipleOfSeventeen(number) {
+  return number % 17 === 0;
 }
+
+module.exports = {
+  isMultipleOfSeventeen
+};
