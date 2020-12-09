88f71e243e7bb7cb8fbf0471289a1b08ced0c3a4
r0ckman123 <vo1dbp2f@icloud.com>
2020-12-09 22:51:10 +0900

    isMultipleOfSeventeenの実装

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
+/** 
+*17の倍数かどうか判定
+*@param {int} seventeen
+*/
+function isMultipleOfSeventeen(seventeen) {
+    return seventeen % 17 === 0 ;
+        
+}
+
 module.exports = {
+    isMultipleOfSeventeen
 }
