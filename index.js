1552b53f46032f1cf5d78b028e75ae55309acd51
Lisa Hirano <risa_hirano@dwango.co.jp>
2020-12-15 11:11:39 +0900

    練習問題の実装

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 'use strict';
-
-module.exports = {
+/**
+ * 17で割り切れる時にtrueを返す
+ * @param {number} num
+ */
+function isMultipleOfSeventeen(num) {
+  return num % 17 === 0;
 }
+
+module.exports = { isMultipleOfSeventeen };
