78c2ceea0a4fccd2754b43896a95126d76e2fd43
proglawm <proglawm@gmail.com>
2021-01-06 06:25:02 +0900

    isMultipleOfSeventeen関数を実装

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 'use strict';
+/**
+ * 引数の数字が17で割り切れるか
+ * @param {number} number
+ * @return {boolean} 17で割り切れるかどうか
+ */
+function isMultipleOfSeventeen(number) {
+  return number % 17 === 0;
+}
+
+// const isMultipleOfSeventeen = (number) => number % 17 === 0;
 
 module.exports = {
+  isMultipleOfSeventeen
 }
