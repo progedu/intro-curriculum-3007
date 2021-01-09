dcfe8e19a03545aebcfbbdee5e44f1e479b885f1
Inori <sknqtvtv@gmail.com>
2021-01-09 09:49:12 +0900

    誤字修正

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 /**
  * 17の倍数をtrueで返す関数
  * @param {number} 数値
- * @returns {bool} 真偽地値(boolean)
+ * @returns {bool} 真偽値(boolean)
  */
 function isMultipleOfSeventeen(number) { // 配列の数値をとりたいので、引数には数値をあらわすnumberを入れた
   return number % 17 === 0; // numberが17で割り切れるか
