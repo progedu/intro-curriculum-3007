2f805484906c90d4cb050f2344e1ba5003593e47
tsubo3306 <tsubocci@gmail.com>
2021-01-14 19:46:18 +0900

    seventeen

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 'use strict';
 
-module.exports = {
+function isMultipleOfSeventeen(num) {
+    if (num % 17 === 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
+
+module.exports = { isMultipleOfSeventeen };
