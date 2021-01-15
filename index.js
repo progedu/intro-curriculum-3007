a3244e102c7dc432c682b060b666a3ae7564c098
harple17 <harpyeagle1706@gmail.com>
2021-01-15 09:37:23 +0000

    create isMultipulOfSeventeen func

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
+/**
+ * 17で割り切れる要素を取り出す
+ * @param {Number} number
+ */
+function isMultipleOfSeventeen(number) {
+    return number%17 === 0;
+}
+
 module.exports = {
+    isMultipleOfSeventeen
 }
