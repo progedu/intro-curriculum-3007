9b9107c3cdf9018ca0706e86ab3e3ff11dbd4efa
ukyon <z5g0323@gmail.com>
2021-01-25 23:08:11 +0900

    isMultipleOfSeventeenを実装

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 'use strict';
+/**
+ * 17で割り切れたらtrue,それ以外はfalse
+ * @param {int} value
+ * @return {boolean} 17で割り切れるかどうか
+ */
+function isMultipleOfSeventeen(value) {
+    if(value%17===0){
+        return true;
+    }else{
+        return false;
+    }
+}
 
 module.exports = {
+    isMultipleOfSeventeen
 }
