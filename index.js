f8fbee18a3e3ff7ea794b18445288d3170447d9a
kiuchi kotaro <sakaakaa42@gmail.com>
2021-02-07 14:39:06 +0900

    q

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
+/**
+ * 17で割り切れるようにする
+ * @param {string} number
+ */
+function isMultipleOfSeventeen(number) {
+  return number % 17 ===0;
+}
+
 module.exports = {
+  isMultipleOfSeventeen
 }
