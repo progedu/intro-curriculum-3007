0c7b91437448812f396306f1b750de24ec987943
Tsurara Ume <garden_suisyo@outlook.jp>
2021-02-09 21:36:01 +0900

    isMultipleOfSeventeen関数を追加

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
+/**
+ * @param {number} num
+ */
+function isMultipleOfSeventeen(num){
+    if (num%17 === 0){
+        return true;
+    }else{
+        return false;
+    }
+}
 
-module.exports = {
+module.exports = { isMultipleOfSeventeen
 }
