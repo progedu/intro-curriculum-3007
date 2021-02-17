66d9ad017e6584a317d28d78e3aee0f633e9bc55
Akira Tanaka <akira223tanaka@gmail.com>
2021-02-17 16:31:00 +0900

    seventeenモジュールの実装

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 'use strict';
-
-module.exports = {
+/**
+ * 17で割り切れる場合 true を返す
+ * @param {number} num 
+ */
+function isMultipleOfSeventeen(num) {
+    return num %17 === 0;
 }
+
+module.exports = { isMultipleOfSeventeen };
