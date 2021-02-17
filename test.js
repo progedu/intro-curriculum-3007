e2a2b80d493c6b87de112dc3cb630393b911db17
Kouhei Okamoto <beruzebuscotty3000@gmail.com>
2021-02-17 22:05:48 +0900

    17で割り切れる数

--- test.js
+++ test.js
@@ -3,6 +3,6 @@ const seventeen = require('./index.js');
 const assert = require('assert');
 
 const a = [83, 32, 85, 47, 77, 8, 61, 74, 29, 34, 11, 76, 60, 99, 55, 7, 19, 60, 98, 38, 28, 96, 32];
-assert.deepEqual(a.filter(seventeen.isMultipleOfSeventeen), [85, 34]);
+assert.deepEqual(a.filter(seventeen.isMultipleOfSeventeen), [77, 7, 98, 28]);
 
 console.log('テストが正常に完了しました');
