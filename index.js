b22296b50edf5d959d6351d7d6c89d2bbdeb5503
kosei yamasaki <kyamasaki95624@gmail.com>
2021-02-26 20:30:57 +0900

    Add isMultipleOfSeventeen function to index.js

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 'use strict';
+const numbers = [];
+
+/**
+ * 整数が17で割り切れるかどうかを判定
+ * @param {number} number
+ * @return {boolean} 割り切れるかどうか
+ */
+function isMultipleOfSeventeen(number) {
+    if (number % 17 === 0)  {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 module.exports = {
+    isMultipleOfSeventeen
 }
