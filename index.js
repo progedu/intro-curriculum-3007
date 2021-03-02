18be925f51e17650f4d2699155a02a719e01aeb2
Toshiya Nosaka <git@lanismile.com>
2021-03-02 14:38:59 +0900

    整数が17で割り切れるかを判定する関数

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
+/**
+ * 配列に含まれる整数が 17 で割り切れる場合、真を返す
+ * @param {number} n
+ */
+function isMultipleOfSeventeen(n) {
+    return n % 17 === 0;
+}
+
 module.exports = {
+    isMultipleOfSeventeen
 }
