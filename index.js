396625113deaadd4daa1fe8dd5f37d98e187b71a
Shun_Harutaro <n.shunshun2004@gmail.com>
2021-03-08 21:20:56 +0900

    isMultipleOfSeventeen関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+/**
+ * seventeenモジュールの実装
+ * @param {number} num
+ */
+
 'use strict';
+function isMultipleOfSeventeen(num){
+  return num % 17 === 0;
+}
 
 module.exports = {
+  isMultipleOfSeventeen
 }
