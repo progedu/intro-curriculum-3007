b60bf1cbcff7dd9eeb54a486d32859e546f35d92
Selno <meitxyps385@gmail.com>
2021-03-13 21:24:53 +0900

    17で整数が割り切れるかどうかを判定する関数をモジュールとして実装しました

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 'use strict';
 
+/**
+ * @param {number} num 
+ * @returns {boolean}
+ */
+function isMultipleOfSeventeen(num) {
+    if (num % 17 === 0) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
 module.exports = {
+    isMultipleOfSeventeen
 }
