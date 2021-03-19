39d1f042cda1b9ad8e8deafdfc8fdfd829f0272b
kenji <roguepurin124@icloud.com>
2021-03-19 15:33:50 +0000

    seventeenモジュール作成

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
+/**
+ * 17で割り切れたらtrueを返す関数
+ * @param {array} num
+ * @return {array} 
+ */
+function isMultipleOfSeventeen(num) {
+  return num % 17 === 0;
+}
+
 module.exports = {
+  isMultipleOfSeventeen : isMultipleOfSeventeen
 }
