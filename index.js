249bbb747a124cf2ef550d52a91156d80a33c9d0
itsu <hiyoro@gmail.com>
2021-03-21 11:45:09 +0900

    数値を17で割り切れる数値ものだけにするモジュールの実装

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
-module.exports = {
+/*
+ * 17で割りきれるかの判定
+ * @return {array} 17で割り切れる数値の配列
+ */
+function isMultipleOfSeventeen(num){
+    return num%17===0;
 }
+
+module.exports = {
+    isMultipleOfSeventeen
+};
