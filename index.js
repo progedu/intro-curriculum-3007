7be7519e6f4594159ef22070c37c304b37c6c6fa
Mizuki Matsui <mizuki.tandf0910@gmail.com>
2021-04-17 19:30:25 +0900

    17で割り切れる

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
+/**
+ * 17の倍数である場合 true を返す
+ * @param {number} n
+ */
+function isMultipleOfSeventeen(n){
+  return n % 17 === 0;
+}
 
 module.exports = {
-}
+  isMultipleOfSeventeen
+};
