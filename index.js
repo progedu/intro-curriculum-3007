ac66887280635419be7332301ec5a4d7bb6ae176
rumis711 <satorumi711@gmail.com>
2021-05-06 17:11:22 +0900

    17で割り切れるものだけにする関数を実装

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
 
+/**１７で割り切れる場合だけtureを返す
+ * @param {number} num
+ */
+function isMultipleOfSeventeen(num){
+    return num % 17 === 0;
+}
+
 module.exports = {
+    isMultipleOfSeventeen
 }
