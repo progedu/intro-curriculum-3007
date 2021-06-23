7fb61b5bbf25f2d5a17e74d7d1b4cef9608c0f7f
Aya <ayaka19n@gmai.com>
2021-06-23 19:13:49 +0900

    seventeen モジュールの実装

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 'use strict';
+/**
+ * 17の倍数をtrueで返す関数
+ * @param {num} 数値
+ * @return {bool}
+ */
+
+function isMultipleOfSeventeen(num){
+  return num % 17 === 0;
+}
 
 module.exports = {
+  isMultipleOfSeventeen
 }
+
+
