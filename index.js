66e446db29f81a916bb5de86f26352f792eef82e
Kento Futatsugami <futatsugami_kento@ideal-ltd.jp>
2021-09-19 00:19:53 +0900

    テスト実装完了

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
+/**
+ * 文字列を17で割れるか確認する。
+ * @return {int} number
+ */
+ function isMultipleOfSeventeen(number) {
+    return number % 17 === 0;
+  }
+  
 module.exports = {
+    isMultipleOfSeventeen
 }
