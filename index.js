fa7d2e9bee90c0cf159b542d466f8d5650bf6b28
Kentaro Kosugi <kosugi.q@gmail.com>
2022-03-30 20:30:32 +0900

    isMultipleOfSeventeen関数をseventeenモジュールに実装

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
+/**
+ * 受け取った整数が17で割り切れるかどうか判定する
+ * @param {number} num
+ * @return {boolean}
+ */
+function isMultipleOfSeventeen(num) {
+    return num % 17 === 0;
+}
+
 module.exports = {
+    isMultipleOfSeventeen
 }
